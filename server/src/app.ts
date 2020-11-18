9c6922aac46a0fb267d874090a7658c6f30f8b47
chanyeong <cyjo9603@naver.com>
2020-11-18 21:27:27 +0900

    Chore #1 : express middleware 설정
    - compression으로 통신 압축 설정
    - hpp, helmet으로 보안 설정
    - morgan으로 서버 기록 설정
    - cors 설정

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,10 +2,20 @@ import { ApolloServer } from 'apollo-server-express';
 import express, { Express } from 'express';
 import { createServer, Server } from 'http';
 import { PubSub } from 'graphql-subscriptions';
+import cors from 'cors';
+import helmet from 'helmet';
+import hpp from 'hpp';
+import compression from 'compression';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
 
 import schema from '@config/schema';
 
+dotenv.config();
+
 const GRAPHQL_ENDPOINT = '/graphql';
+
+const prod = process.env.NODE_ENV === 'production';
 
 class App {
   public app: Express;
@@ -29,6 +39,27 @@ class App {
   }
 
   private middlewares() {
+    if (prod) {
+      this.app.use(helmet());
+      this.app.use(hpp());
+      this.app.use(morgan('combined'));
+      this.app.use(
+        cors({
+          origin: /ikeytax\.tk$/,
+          credentials: true,
+        }),
+      );
+    } else {
+      this.app.use(morgan('dev'));
+      this.app.use(
+        cors({
+          origin: true,
+          credentials: true,
+        }),
+      );
+    }
+
+    this.app.use(compression());
     this.apolloServer.applyMiddleware({
       app: this.app,
       path: GRAPHQL_ENDPOINT,
