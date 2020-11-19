b0846cf7d2ea94a954f80c6ceadf01aa78f81412
hoo00nn <hoo00nn@naver.com>
2020-11-19 19:00:29 +0900

    Chore #2 : apollo client 설정
    - apollo, react 설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,20 +1,24 @@
 import React, { FC } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import { ApolloProvider } from '@apollo/react-hooks'
 
 import Home from 'routes/Home';
 import theme from 'theme';
 import GlobalStyle from 'theme/global';
+import client from './apollo';
 
 const App: FC = () => (
-  <ThemeProvider theme={theme}>
-    <GlobalStyle />
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-      </Switch>
-    </BrowserRouter>
-  </ThemeProvider>
+  <ApolloProvider client={client} >
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
+  </ApolloProvider>
 );
 
 export default App;
--- client/src/routes/Home/Home.tsx
+++ client/src/routes/Home/Home.tsx
@@ -1,5 +1,22 @@
 import React, { FC } from 'react';
+import { useQuery } from '@apollo/react-hooks';
 
-const Home: FC = () => <>Home</>;
+import { helloQuery } from 'queries/hello';
+
+const Home: FC = () => {
+  const { loading, data, error } = useQuery(helloQuery);
+
+  console.log(data);
+
+  if (loading) return <>loading</>;
+
+  if (error) return <>error</>;
+
+  return (
+    <>
+      Home<span>{data.hello.result}</span>
+    </>
+  );
+};
 
 export default Home;
