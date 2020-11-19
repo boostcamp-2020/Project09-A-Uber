ab576cb032b6f988767dcb36e8c1b6412280db85
chanyeong <cyjo9603@naver.com>
2020-11-19 22:07:54 +0900

    Chore #2 : client graphql type generate 설정
    - apollo codegen을 사용해 자동으로 code generate 설정

--- client/src/routes/Home/Home.tsx
+++ client/src/routes/Home/Home.tsx
@@ -1,30 +1,5 @@
 import React, { FC } from 'react';
-import { useQuery } from '@apollo/react-hooks';
-import { useDispatch, useSelector } from 'react-redux';
 
-import { helloQuery } from 'queries/hello';
-import { InitialState } from 'reducers';
-import addUserInfo from 'reducers/addUserInfo';
-
-const Home: FC = () => {
-  const dispatch = useDispatch();
-  const user = useSelector((state: InitialState) => state.user);
-  const { loading, data, error } = useQuery(helloQuery);
-
-  const onClick = () => {
-    dispatch(addUserInfo('test string'));
-  };
-
-  if (loading) return <>loading</>;
-
-  if (error) return <>error</>;
-
-  return (
-    <>
-      <button onClick={onClick}></button>
-      Home<span>{data.hello.result}</span>
-    </>
-  );
-};
+const Home: FC = () => <>Home</>;
 
 export default Home;
