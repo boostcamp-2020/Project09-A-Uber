3ae3f14f04f1768da8cc524464e30e22a2feb05e
hoo00nn <hoo00nn@naver.com>
2020-11-23 16:26:00 +0900

    Feat #14 : 로그인 인증 구현
    - passport local 전략 사용
    - 로그인 성공 시 refresh token 유저 DB에 저장하는 로직 추가해야함
    - mock 데이터로 테스트 완료

---
+++ server/src/passport/auth.ts
@@ -0,0 +1,33 @@
+import passport from '@passport/index';
+import jwt from 'jsonwebtoken';
+import { ExpressFunction } from '@type/ExpressFunction';
+
+interface Message {
+  message: string;
+}
+
+interface SECRET_KEY {
+  JWT_SECRET_KEY?: string | undefined;
+}
+
+const { JWT_SECRET_KEY }: SECRET_KEY = process.env;
+
+const loginAuth: ExpressFunction = async (req, res, next) => {
+  try {
+    passport.authenticate('local', (error, user, { message }: Message) => {
+      if (error || !user) return res.status(400).json({ result: 'fail', message });
+
+      const payload = { email: user.email };
+      const AccessToken = jwt.sign(payload, JWT_SECRET_KEY!);
+      const RefreshToken = jwt.sign(payload, JWT_SECRET_KEY!);
+
+      // 리프레시 토큰 서버에 저장 로직 추가 필요
+
+      res.json({ result: 'success', AccessToken });
+    })(req, res, next);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default loginAuth;
