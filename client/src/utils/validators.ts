2704c04f7571131edd570cd7e7ebbd4bfcb0e68a
김종은 <jongeunkim@gimjong-eun-ui-MacBookPro.local>
2020-11-24 02:42:49 +0900

    Feat #10 : 결제정보 유효성 검사 함수 작성
    카드번호, 만료일자, cvc에 대한 입력 값에 정규식 검사하는 함수 생성

---
+++ client/src/utils/validators.ts
@@ -0,0 +1,26 @@
+interface RegExpDictionary {
+  [key: string] : RegExp;
+}
+
+const regExp: RegExpDictionary = {
+  cardNumber: /^\d{4}$/,
+  expiryDate: /^(0[1-9]|1[0-2])[/](01|[0][1-9]|[1-2][1-9]|[3][0-1])$/,
+  cvcNumber: /^\d{3}$/,
+};
+
+interface CardNumber {cardNumber1 : string, cardNumber2: string, cardNumber3: string, cardNumber4 : string}
+
+export const isCardNumber = (maybeCardNumber : CardNumber) : boolean => {
+  return regExp.cardNumber.test(maybeCardNumber.cardNumber1) && 
+  regExp.cardNumber.test(maybeCardNumber.cardNumber2) && 
+  regExp.cardNumber.test(maybeCardNumber.cardNumber3) && 
+  regExp.cardNumber.test(maybeCardNumber.cardNumber4);
+}
+
+export const isExpiryDate = (maybeExpiryDate: string) : boolean => {
+  return regExp.expiryDate.test(maybeExpiryDate);
+}
+
+export const isCVCNumber = (maybeCVC: string) : boolean => {
+  return regExp.cvcNumber.test(maybeCVC);
+}
