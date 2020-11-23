8de2bf0b6c830b8468df3f177c7eca1ba50793ee
kimhakjun <dkd7142@naver.com>
2020-11-23 11:08:28 +0900

    Style #noissue : 전역 스타일 설정
    - font-size, height 전역 설정
    - 앱에서 사용될 전역 Page Fram 컴포넌트 작성

---
+++ client/src/components/PageFrame/PageFrame.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+
+import styled from '@theme/styled';
+
+const StyledPageFrame = styled.section`
+  display: flex;
+  flex-direction: column;
+  padding: 1rem 1.5rem 2rem 1.5rem;
+  height: 100%;
+`;
+
+export default StyledPageFrame;
--- client/src/theme/global.ts
+++ client/src/theme/global.ts
@@ -6,8 +6,18 @@ const GlobalStyle = createGlobalStyle`
 
   @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap');
 
+  * {
+    box-sizing: border-box;
+  }
+
   body {
     font-family: 'Noto Sans KR', sans-serif;
+    font-size: 14px;
+    height: 100% ;
+  }
+
+  html, #root {
+    height: 100%;
   }
 `
 export default GlobalStyle
