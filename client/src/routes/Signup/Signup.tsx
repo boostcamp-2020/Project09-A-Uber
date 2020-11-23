2e66442807cca65e38e2a5285bfdc4fb5e9bf7e8
chanyeong <cyjo9603@naver.com>
2020-11-23 16:22:33 +0900

    Feat #7 : 회원가입 페이지 타이틀 및 토글 추가
    - 회원가입 페이지에 타이틀 및 토글 추가
    - 토글 상태 및 선택함수 구현

--- client/src/routes/Signup/Signup.tsx
+++ client/src/routes/Signup/Signup.tsx
@@ -1,7 +1,45 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useCallback } from 'react';
+
+import styled from '@theme/styled';
 
 import PageFrame from '@components/PageFrame';
+import Toggle, { ToggleFocus, FOCUS_USER } from '@/components/UserToggle';
 
-const Signup: FC = () => <PageFrame></PageFrame>;
+const StyledSignup = styled(PageFrame)`
+  & .signup-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-end;
+
+    & h1 {
+      font-size: 2.2rem;
+      font-weight: 700;
+      color: ${({ theme }) => theme.PRIMARY};
+    }
+  }
+`;
+
+const Signup: FC = () => {
+  const [signupTarget, setSignupTarget] = useState<ToggleFocus>(FOCUS_USER);
+
+  const onClickToggleHandler = useCallback(
+    (target: ToggleFocus) => {
+      if (target === signupTarget) {
+        return;
+      }
+      setSignupTarget(target);
+    },
+    [signupTarget],
+  );
+
+  return (
+    <StyledSignup>
+      <div className="signup-header">
+        <h1>회원가입</h1>
+        <Toggle focus={signupTarget} onClick={onClickToggleHandler} />
+      </div>
+    </StyledSignup>
+  );
+};
 
 export default Signup;
