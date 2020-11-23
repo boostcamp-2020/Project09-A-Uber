a007b34f5d0a2dd898eccf66d41238e7b13ca563
hoo00nn <hoo00nn@naver.com>
2020-11-24 01:24:13 +0900

    Chore #noissue : user 스키마 unique 속성 추가
    - email에 unique 속성 추가
    - 동일한 email로 여러 사용자가 가입하는거 방지

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -2,7 +2,7 @@ import { Schema, model } from 'mongoose';
 
 const userSchema = new Schema({
   id: Schema.Types.ObjectId,
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   name: { type: String, required: true },
   phone: { type: String, required: true },
