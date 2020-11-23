f638a4302900646a3a6bcc9d6fbaae15c4d8f18a
hoo00nn <hoo00nn@naver.com>
2020-11-24 00:57:59 +0900

    Feat #30 : mock 데이터 삭제
    - mock 데이터 삭제
    - 로그인 인증 시 DB에서 값 비교하도록 변경

--- server/src/passport/local.ts
+++ server/src/passport/local.ts
@@ -1,21 +1,21 @@
 import { Strategy as LocalStrategy, VerifyFunction } from 'passport-local';
+
 import { isComparedPassword } from '@util/bcrypt';
 import { Message } from '@util/server-message';
+import UserModel from '@models/user';
 
 const config = { usernameField: 'email', passwordField: 'password' };
-const mock = {
-  email: 'gildong@test.com',
-  password: '$2b$10$UzWKr4Em04s.UVjHZf1fHe8rc39voVNMuUhVCW7KP.tc0vrbmNW.K',
-};
 
 const authenticate: VerifyFunction = async (email: string, password: string, done) => {
   try {
-    if (mock.email !== email) return done(null, false, { message: Message.InvalidEmail });
-    if (!isComparedPassword(password, mock.password)) {
+    const user = await UserModel.findOne({ email });
+
+    if (!user) return done(null, false, { message: Message.InvalidEmail });
+    if (!isComparedPassword(password, user?.get('password'))) {
       return done(null, false, { message: Message.InvalidPassword });
     }
 
-    return done(null, mock, { message: Message.SucceedLogin });
+    return done(null, user, { message: Message.SucceedLogin });
   } catch (error) {
     done(error);
   }
