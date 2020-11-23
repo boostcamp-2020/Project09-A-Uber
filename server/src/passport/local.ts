f6f007ae36f207d8080be81086be9104b05b3707
hoo00nn <hoo00nn@naver.com>
2020-11-24 01:35:29 +0900

    Feat #30 : 로그인 유형 체크하는 로직 추가
    - 일반 사용자, 드라이버 중 어떤 것으로 로그인 했는지 체크하는 로직 추가

--- server/src/passport/local.ts
+++ server/src/passport/local.ts
@@ -1,14 +1,33 @@
-import { Strategy as LocalStrategy, VerifyFunction } from 'passport-local';
+import {
+  Strategy as LocalStrategy,
+  VerifyFunctionWithRequest,
+  IStrategyOptionsWithRequest,
+} from 'passport-local';
+import { Request } from 'express';
 
 import { isComparedPassword } from '@util/bcrypt';
 import { Message } from '@util/server-message';
 import UserModel from '@models/user';
 
-const config = { usernameField: 'email', passwordField: 'password' };
+interface LoginType {
+  loginType: '일반 사용자' | '드라이버';
+}
 
-const authenticate: VerifyFunction = async (email: string, password: string, done) => {
+const config: IStrategyOptionsWithRequest = {
+  passReqToCallback: true,
+  usernameField: 'email',
+  passwordField: 'password',
+};
+
+const authenticate: VerifyFunctionWithRequest = async (
+  req: Request,
+  email: string,
+  password: string,
+  done,
+) => {
   try {
-    const user = await UserModel.findOne({ email });
+    const { loginType }: LoginType = req.body;
+    const user = await UserModel.findOne({ email, type: loginType });
 
     if (!user) return done(null, false, { message: Message.InvalidEmail });
     if (!isComparedPassword(password, user?.get('password'))) {
