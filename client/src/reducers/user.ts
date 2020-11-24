ff986205c0dede4867833d6db80b15f8a4261b05
hoo00nn <hoo00nn@naver.com>
2020-11-24 22:22:48 +0900

    Feat #40 : 로그인 액션 정의
    - saga에서 사용할 로그인 액션 정의
    - 로그인 요청, 성공, 실패 3가지 액션으로 정의함

--- client/src/reducers/user.ts
+++ client/src/reducers/user.ts
@@ -4,12 +4,22 @@ export const SIGN_UP_REQUEST = 'SIGN_UP_REQUEST';
 export const SIGN_UP_SUCCESS = 'SIGN_UP_SUCCESS';
 export const SIGN_UP_FAILURE = 'SIGN_UP_FAILURE';
 
+export const SIGN_IN_REQUEST = 'SIGN_IN_REQUEST';
+export const SIGN_IN_SUCCESS = 'SIGN_IN_SUCCESS';
+export const SIGN_IN_FAILURE = 'SIGN_IN_FAILURE';
+
 interface UserCommonInfo {
   email: string;
   name: string;
   password: string;
   phone: string;
   type: ToggleFocus;
+}
+
+export interface LoginInfo {
+  email: string;
+  password: string;
+  loginType: string;
 }
 
 export interface UserInfo extends UserCommonInfo {
@@ -43,7 +53,21 @@ export interface SignUpFailure {
   error: string;
 }
 
-// 회원가입 액션 크링
+export interface SignInRequest {
+  type: typeof SIGN_IN_REQUEST;
+  data: LoginInfo;
+}
+
+export interface SignInSuccess {
+  type: typeof SIGN_IN_SUCCESS;
+}
+
+export interface SignInFailure {
+  type: typeof SIGN_IN_FAILURE;
+  error: string;
+}
+
+// 회원가입 액션
 export const signUpRequest = (data: UserInfo | DriverInfo): SignUpRequest => ({
   type: SIGN_UP_REQUEST,
   data,
@@ -51,5 +75,17 @@ export const signUpRequest = (data: UserInfo | DriverInfo): SignUpRequest => ({
 export const signUpSuccess = (): SignUpSuccess => ({ type: SIGN_UP_SUCCESS });
 export const signUpFailure = (error: string): SignUpFailure => ({
   type: SIGN_UP_FAILURE,
+  error,
+});
+
+// 로그인 액션
+export const signInRequest = (data: LoginInfo): SignInRequest => ({
+  type: SIGN_IN_REQUEST,
+  data,
+});
+
+export const signInSuccess = (): SignInSuccess => ({ type: SIGN_IN_SUCCESS });
+export const signInFailure = (error: string): SignInFailure => ({
+  type: SIGN_IN_FAILURE,
   error,
 });
