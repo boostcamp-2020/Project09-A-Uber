77bf08e435f44dd0c637bfe76a51e1132fffb85d
jongeunk0613 <gladiolus4231@gmail.com>
2020-11-24 13:41:40 +0900

    Refactor #10 #18 : 유효성 검사 함수 수정
    운전자 입력 정보의 유효성 검사를 진행할 때 시작과 끝점을 명시해서 넘어가는 인풋도 거르도록 설정.
    4개의 4자리수의 카드번호를 한번에 정규식 검사를 하던것을 4자리 수 하나만 검사하도록 수정.

--- client/src/utils/validators.ts
+++ client/src/utils/validators.ts
@@ -6,24 +6,12 @@ const regExp: RegExpDictionary = {
   cardNumber: /^\d{4}$/,
   expiryDate: /^(0[1-9]|1[0-2])[/](01|[0][1-9]|[1-2][1-9]|[3][0-1])$/,
   cvcNumber: /^\d{3}$/,
-  carNumber: /\d{1,3}[가-힣]\s\d{4}/,
-  license: /\d{2}-\d{2}-\d{6}-\d{2}/,
+  carNumber: /^\d{1,3}[가-힣]\s\d{4}$/,
+  license: /^\d{2}-\d{2}-\d{6}-\d{2}$/,
 };
 
-interface CardNumber {
-  cardNumber1: string;
-  cardNumber2: string;
-  cardNumber3: string;
-  cardNumber4: string;
-}
-
-export const isCardNumber = (maybeCardNumber: CardNumber): boolean => {
-  return (
-    regExp.cardNumber.test(maybeCardNumber.cardNumber1) &&
-    regExp.cardNumber.test(maybeCardNumber.cardNumber2) &&
-    regExp.cardNumber.test(maybeCardNumber.cardNumber3) &&
-    regExp.cardNumber.test(maybeCardNumber.cardNumber4)
-  );
+export const isCardNumber = (maybeCardNumber: string): boolean => {
+  return regExp.cardNumber.test(maybeCardNumber);
 };
 
 export const isExpiryDate = (maybeExpiryDate: string): boolean => {
