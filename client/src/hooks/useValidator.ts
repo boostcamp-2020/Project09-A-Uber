bd81e8f1b1825227e6071b1c1c4b0b32c81562df
hoo00nn <hoo00nn@naver.com>
2020-11-24 11:29:44 +0900

    Feat #noissue : useValidator 커스텀 훅 추가
    - 유효성 검사 할 수 있는 커스텀 훅 추가함

---
+++ client/src/hooks/useValidator.ts
@@ -0,0 +1,29 @@
+import { useState, useCallback } from 'react';
+
+export default <T extends { value: string } = HTMLInputElement>(
+  initialState: string,
+  validator: (value: string) => boolean,
+  limitLength?: number,
+): [
+  string,
+  React.Dispatch<React.SetStateAction<string>>,
+  (e: React.ChangeEvent<T>) => void,
+  boolean,
+] => {
+  const [value, setValue] = useState(initialState);
+  const [isValid, setIsValid] = useState(false);
+
+  const onChangeHandler = useCallback(
+    (e: React.ChangeEvent<T>) => {
+      if (!limitLength || (limitLength && value.length <= limitLength)) {
+        setValue(e.target.value);
+      }
+
+      if (validator(value)) setIsValid(true);
+      else setIsValid(false);
+    },
+    [value],
+  );
+
+  return [value, setValue, onChangeHandler, isValid];
+};
--- client/src/utils/validators.ts
+++ client/src/utils/validators.ts
@@ -6,6 +6,8 @@ const regExp: RegExpDictionary = {
   cardNumber: /^\d{4}$/,
   expiryDate: /^(0[1-9]|1[0-2])[/](01|[0][1-9]|[1-2][1-9]|[3][0-1])$/,
   cvcNumber: /^\d{3}$/,
+  carNumber: /\d{1,3}[가-힣]\s\d{4}/,
+  license: /\d{2}-\d{2}-\d{6}-\d{2}/,
 };
 
 interface CardNumber {
@@ -30,4 +32,12 @@ export const isExpiryDate = (maybeExpiryDate: string): boolean => {
 
 export const isCVCNumber = (maybeCVC: string): boolean => {
   return regExp.cvcNumber.test(maybeCVC);
+};
+
+export const isCarNumber = (maybeCarNumber: string): boolean => {
+  return regExp.carNumber.test(maybeCarNumber);
+};
+
+export const isLicense = (maybeLicense: string): boolean => {
+  return regExp.license.test(maybeLicense);
 };
