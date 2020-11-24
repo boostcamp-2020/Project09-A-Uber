db005b96e566aa48ba641975fa20e0a142179756
jongeunk0613 <gladiolus4231@gmail.com>
2020-11-24 13:45:21 +0900

    Refactor #20 : Driver Form에 useValidator 사용
    - useValidator를 이용해서 입력값의 유효성 검사 실시
    - allow 속성을 추가해서 입력된 값이 맞는지 유저가 확인할 수 있도록 구현

--- client/src/components/DriverForm/DriverForm.tsx
+++ client/src/components/DriverForm/DriverForm.tsx
@@ -1,10 +1,11 @@
-import React, { FC, useCallback, useState } from 'react';
+import React, { FC, useCallback } from 'react';
 import { Button } from 'antd-mobile';
 import styled from '@theme/styled';
 import Selector from '@components/Selector';
 import Input from '@components/Input';
-import useChange from '@/hooks/useChange';
-import { isCarNumber, isLicense } from '../../utils/vaildator';
+import useChange from '@hooks/useChange';
+import useValidator from '@hooks/useValidator';
+import { isCarNumber, isLicense } from '@utils/validators';
 
 interface Props {
   name: string;
@@ -33,10 +34,10 @@ const StyledDriverForm = styled.div`
 
 const carTypes: string[] = ['대형', '중형', '소형'];
 
-const DriverForm: FC<Props> = ({ name, email, password, phone }) => {
+const DriverForm: FC = () => {
   const [carType, , onChangeCarType] = useChange<HTMLSelectElement>('');
-  const [carNumber, , onChangeCarNumber] = useChange('');
-  const [lisence, , onChangeLisence] = useChange('');
+  const [carNumber, , onChangeCarNumber, isCarNumValid] = useValidator('', isCarNumber);
+  const [lisence, , onChangeLisence, isLisence] = useValidator('', isLicense);
 
   const onSubmit = useCallback(
     (e: React.FormEvent) => {
@@ -58,17 +59,21 @@ const DriverForm: FC<Props> = ({ name, email, password, phone }) => {
       />
       <Input
         title="차량번호"
-        placeholder="차량번호를 입력해주세요."
+        placeholder="차량번호를 입력해주세요. 예시) 07나 1247"
         value={carNumber}
         onChange={onChangeCarNumber}
+        allow={isCarNumValid}
       />
       <Input
         title="운전면허 번호"
-        placeholder="운전면허 번호 입력해주세요."
+        placeholder="운전면허 번호 입력해주세요. 12-12-123456-12"
         value={lisence}
         onChange={onChangeLisence}
+        allow={isLisence}
       />
-      <Button type="primary" onClick={onSubmit}>회원가입</Button>
+      <Button type="primary" onClick={onSubmit}>
+        회원가입
+      </Button>
     </StyledDriverForm>
   );
 };
