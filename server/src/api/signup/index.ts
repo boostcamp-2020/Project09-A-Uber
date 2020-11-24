4eadd141a15968238303ea4500478c757e7f09ae
chanyeong <cyjo9603@naver.com>
2020-11-24 15:00:04 +0900

    Feat #8 : 드라이버 회원가입 API 구현
    - 각 항목(name, password, email, phone, driver)이 필수로 들어가야 회원가입 되는 지 확인
    - 드라이버 상태값에 기본 값 지정
    - 드라이버 회원가입 라우터 등록

--- server/src/api/signup/index.ts
+++ server/src/api/signup/index.ts
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 
 import userSignupAPI from './userSignup';
+import driverSignupAPI from './driverSignup';
 
 const router = Router();
 
 router.post('/user', userSignupAPI);
+
+router.post('/driver', driverSignupAPI);
 
 export default router;
