508d900d7ea893f738f3f3e3ca03c6ce88067ad4
jongeunk0613 <gladiolus4231@gmail.com>
2020-11-24 10:40:20 +0900

    Refactor #11 : 로직을 한줄로 줄임
    인풋의 글자수를 제한하는 로직을 ?연산자를 사용해서 한줄이 되도록 줄여서 수정하였다.

--- client/src/utils/restrictors.ts
+++ client/src/utils/restrictors.ts
@@ -1,4 +1,3 @@
 export const restrictInputLength = (value: string, length: number): string => {
-  if (value.length > length) return value.substring(0, length);
-  return value;
+  return value.length > length ? value.substring(0, length) : value;
 };
