2441e500dc4aef846bf609d162042f9b5c3b4e52
김종은 <jongeunkim@gimjong-eun-ui-MacBookPro.local>
2020-11-24 10:39:09 +0900

    Style #noissue : prettier 적용 안된던 것 수정
    prettier가 적용이 안되고 있어서 settings 파일에
    설정을  추가해서 해결했다.

--- client/src/utils/restrictors.ts
+++ client/src/utils/restrictors.ts
@@ -1,5 +1,4 @@
-export const restrictInputLength = (value : string, length : number) : string => {
-  if (value.length > length)
-    return value.substring(0, length);
+export const restrictInputLength = (value: string, length: number): string => {
+  if (value.length > length) return value.substring(0, length);
   return value;
-}
+};
