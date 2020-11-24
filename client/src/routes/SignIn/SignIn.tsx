078eee691feeaf613d8ecf4b446cca1056bf863c
hoo00nn <hoo00nn@naver.com>
2020-11-24 23:53:11 +0900

    Feat #40 : 로그인 실패 시 alert 추가
    - 입력 정보가 올바르지 않습니다 라는 메세지 띄워줌

--- client/src/routes/SignIn/SignIn.tsx
+++ client/src/routes/SignIn/SignIn.tsx
@@ -1,12 +1,15 @@
-import React, { FC, useState, useCallback } from 'react';
+import React, { FC, useState, useCallback, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { SegmentedControl, Button, Checkbox } from 'antd-mobile';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { StyledSignIn } from '@routes/SignIn/style';
 import StyledPageFrame from '@components/PageFrame';
 import logo from '@images/logo.svg';
 import Input from '@components/Input';
 import useChange from '@hooks/useChange';
+import { InitialState } from '@reducers/.';
+import { signInRequest } from '@reducers/user';
 
 const visitorType = ['일반 사용자', '드라이버'];
 const initialVisitorType = '일반 사용자';
@@ -17,15 +20,34 @@ const SignIn: FC = () => {
   const [isLoginState, setIsLoginState] = useState(false);
   const [email, setEmail, onChangeEmail] = useChange('');
   const [password, setPassword, onChangePassword] = useChange('');
+  const dispatch = useDispatch();
+  const {
+    signin: { result },
+  } = useSelector((state: InitialState) => state);
   const onClickSignup = useCallback(() => {
     history.push('/signup');
   }, []);
+  const onClickSignIn = useCallback(() => {
+    dispatch(
+      signInRequest({
+        email,
+        password,
+        loginType,
+      }),
+    );
+  }, [email, password, loginType]);
   const onChangeType = useCallback((value: string) => {
     setLoginType(value);
   }, []);
   const onChangeLoginState = useCallback(() => {
     setIsLoginState(!isLoginState);
   }, []);
+
+  useEffect(() => {
+    if (result) {
+      history.push('/');
+    }
+  }, [result]);
 
   return (
     <StyledPageFrame>
@@ -44,7 +66,9 @@ const SignIn: FC = () => {
         <div className="login-state">
           <Checkbox onChange={onChangeLoginState}>로그인 상태 유지</Checkbox>
         </div>
-        <Button type="primary">로그인</Button>
+        <Button type="primary" onClick={onClickSignIn}>
+          로그인
+        </Button>
         <Button className="signup-button" onClick={onClickSignup}>
           회원가입
         </Button>
