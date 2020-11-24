7fdda7a22249dcddefaca0c5bb2e9e0b3d4def98
hoo00nn <hoo00nn@naver.com>
2020-11-24 23:40:38 +0900

    Feat #40 : 로그인 액션 재정의
    - saga에서 사용할 로그인 액션 정의
    - 로그인 요청, 성공, 실패 3가지 액션으로 정의함
    - signup 액션 삭제 (머지할 때 합쳐질 예정)

--- client/src/reducers/index.ts
+++ client/src/reducers/index.ts
@@ -1,61 +1,32 @@
-import {
-  SIGN_IN_REQUEST,
-  SIGN_IN_SUCCESS,
-  SIGN_IN_FAILURE,
-  SIGN_UP_REQUEST,
-  SIGN_UP_SUCCESS,
-  SIGN_UP_FAILURE,
-} from './user';
-
-interface Driver {
-  licenseNumber: string;
-  status: string;
-}
+import { SIGN_IN_REQUEST, SIGN_IN_SUCCESS, SIGN_IN_FAILURE } from './user';
 
 export interface InitialState {
-  signUpLoading: boolean;
-  signUpError?: string | null;
-  signInLoading: boolean;
-  signInError?: string | null;
-  user?: {
-    _id: string;
-    name: string;
-    driver?: Driver;
+  signin: {
+    loading: boolean;
+    result?: boolean;
+    error?: string;
   };
 }
 
 const initialState: InitialState = {
-  signUpLoading: false,
-  signUpError: null,
-  signInLoading: false,
-  signInError: null,
+  signin: {
+    loading: false,
+  },
 };
 
 type Action = {
-  type:
-    | typeof SIGN_UP_REQUEST
-    | typeof SIGN_UP_SUCCESS
-    | typeof SIGN_UP_FAILURE
-    | typeof SIGN_IN_REQUEST
-    | typeof SIGN_IN_SUCCESS
-    | typeof SIGN_IN_FAILURE;
+  type: typeof SIGN_IN_REQUEST | typeof SIGN_IN_SUCCESS | typeof SIGN_IN_FAILURE;
   error?: string;
 };
 
 const reducer = (state: InitialState = initialState, action: Action): InitialState => {
   switch (action.type) {
-    case SIGN_UP_REQUEST:
-      return { ...initialState, signUpLoading: true };
-    case SIGN_UP_SUCCESS:
-      return { ...initialState, signUpLoading: false };
-    case SIGN_UP_FAILURE:
-      return { ...initialState, signUpLoading: false, signUpError: action.error };
     case SIGN_IN_REQUEST:
-      return { ...initialState, signInLoading: true };
+      return { ...initialState, signin: { loading: true } };
     case SIGN_IN_SUCCESS:
-      return { ...initialState, signInLoading: false };
+      return { ...initialState, signin: { loading: false, result: true } };
     case SIGN_IN_FAILURE:
-      return { ...initialState, signInLoading: false, signInError: action.error };
+      return { ...initialState, signin: { loading: false, error: action.error } };
     default:
       return state;
   }
