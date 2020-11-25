8deccea60ce22571cb40cea310f8909fecee3ad7
김종은 <jongeunkim@gimjong-eun-ui-MacBookPro.local>
2020-11-25 15:17:07 +0900

    Feat #44 : 유저 정보를 반환하는 resolver 작성
    유저 정보를 조회하는 resolver 작성

---
+++ server/src/api/user/getUserInfo/getUserInfo.resolvers.ts
@@ -0,0 +1,19 @@
+import { Resolvers } from '@type/api';
+
+import getUser from '@services/user/getUser';
+
+const resolvers: Resolvers = {
+  Query: {
+    getUserInfo: async (_, __, { req }) => {
+      const { user, error } = await getUser(req.user || '');
+
+      if (error) {
+        return { error };
+      }
+
+      return { user };
+    },
+  },
+};
+
+export default resolvers;
