6b952c2f9b3728a0fb8e0fb731c6d20d08c42641
kimhakjun <dkd7142@naver.com>
2020-11-25 15:26:12 +0900

    Feat #45 : 구글맵초기 세팅
    - 구글맵 api 키 dot.env 에저장
    - google Map 컴포넌트 생성 및 화면에 띄워지는지 테스트

--- client/src/routes/Home/Home.tsx
+++ client/src/routes/Home/Home.tsx
@@ -1,5 +1,11 @@
 import React, { FC } from 'react';
+import GoogleMap from '@components/GoogleMap';
 
-const Home: FC = () => <>Home</>;
-
+const Home: FC = () => {
+  return (
+    <>
+      <GoogleMap />
+    </>
+  );
+};
 export default Home;
