7f062af19b4b6ee4047c0879a65bcd1117279033
jongeunk0613 <gladiolus4231@gmail.com>
2020-11-26 11:57:50 +0900

    Feat #59 : 메뉴 헤더에 메뉴 생성
    - 메뉴 헤더의 메뉴 버튼을 클릭하면 slide도 메뉴가 보이도록 구현

--- client/src/components/HeaderWithMenu/HeaderWithMenu.tsx
+++ client/src/components/HeaderWithMenu/HeaderWithMenu.tsx
@@ -1,14 +1,14 @@
 import React, { FC } from 'react';
+import useToggle from '@hooks/useToggle';
 
 import styled from '@theme/styled';
 import MenuSVG from '@images/menuSVG.tsx';
 
 interface Props {
-  onClick: () => void;
   className?: string;
 }
 
-const StyledHeaderWithMenu = styled.header`
+const StyledHeaderWithMenu = styled.header<{ isMenuOpen: boolean }>`
   display: flex;
   height: 3rem;
   width: 100%;
@@ -26,14 +26,37 @@ const StyledHeaderWithMenu = styled.header`
     fill: #ffffff;
     margin-left: 1.5rem;
   }
+
+  & menu {
+    position: absolute;
+    top: 3rem;
+    left: 0;
+    visibility: ${(props) => (props.isMenuOpen ? 'visible' : 'hidden')};
+    width: ${(props) => (props.isMenuOpen ? '100%' : '0')};
+    height: calc(100vh - 3rem);
+    z-index: 1;
+    padding: 1rem 1rem;
+    background-color: #ffffff;
+    border-right: 1px solid ${({ theme }) => theme.PRIMARY};
+    transition: 0.5s;
+  }
 `;
 
-const HeaderWithMenu: FC<Props> = ({ onClick, className }) => (
-  <StyledHeaderWithMenu className={className}>
-    <button type="button" onClick={onClick}>
-      <MenuSVG />
-    </button>
-  </StyledHeaderWithMenu>
-);
+const HeaderWithMenu: FC<Props> = ({ className }) => {
+  const [menu, toggleMenu] = useToggle(false);
+
+  return (
+    <StyledHeaderWithMenu className={className} isMenuOpen={menu}>
+      <button type="button" onClick={toggleMenu}>
+        <MenuSVG />
+      </button>
+      <menu>
+        <ul>
+          <li>마이페이지</li>
+        </ul>
+      </menu>
+    </StyledHeaderWithMenu>
+  );
+};
 
 export default HeaderWithMenu;
