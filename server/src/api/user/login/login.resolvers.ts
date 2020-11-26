d668125b55576a714ef02b947f571e58d2219118
hoo00nn <hoo00nn@naver.com>
2020-11-26 10:48:17 +0900

    FIX #noissue : 쿠키 유효기간 설정 변경
    - expires -> maxage 속성으로 변경
    - 날짜 포맷으로 지정해주는 것보다 ms단위로 지정하는 것이 더 편해서 변경함

--- server/src/api/user/login/login.resolvers.ts
+++ server/src/api/user/login/login.resolvers.ts
@@ -5,7 +5,7 @@ import UserModel from '@models/user';
 const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY as string;
 const JWT_HEADER = process.env.JWT_HEADER as string;
 
-const EXPIRED = 1000 * 60 * 15;
+const EXPIRED = 1000 * 60 * 60 * 24 * 14;
 
 const resolvers: Resolvers = {
   Mutation: {
@@ -15,14 +15,14 @@ const resolvers: Resolvers = {
       if (!user) return { result: 'fail', error: info?.message };
       const userId = user?.get('_id');
       const payload = { id: userId };
-      const accessToken = jwt.sign(payload, JWT_SECRET_KEY, { expiresIn: '15m' });
+      const accessToken = jwt.sign(payload, JWT_SECRET_KEY, { expiresIn: '3s' });
       const refreshToken = jwt.sign(payload, JWT_SECRET_KEY, { expiresIn: '14d' });
 
       await UserModel.updateOne({ _id: userId }, { refreshToken });
 
       res.cookie(JWT_HEADER, accessToken, {
         httpOnly: true,
-        expires: new Date(Date.now() + EXPIRED),
+        maxAge: EXPIRED,
       });
 
       return { result: 'success' };
