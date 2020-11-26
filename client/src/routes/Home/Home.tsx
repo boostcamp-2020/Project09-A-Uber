70f08679238a24d58ab69f65233989696c9d50cd
kimhakjun <dkd7142@naver.com>
2020-11-27 02:33:29 +0900

    Feat #66 : 장소검색, 라이더 검색 버튼 컴포넌트 생성
    - HOME 페이지에 장소검색, 버튼 컴포넌트 배치

--- client/src/routes/Home/Home.tsx
+++ client/src/routes/Home/Home.tsx
@@ -1,10 +1,26 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import GoogleMap from '@components/GoogleMap';
+import { Button } from 'antd-mobile';
+
+import AutoLocation from '@components/AutoLocation';
+import { StyledMapWrapper, StyledLocation, StyledMainContainer } from './style';
 
 const Home: FC = () => {
+  const [origin, setOrigin] = useState({ lat: 51.507351, lng: -0.127758 });
+  const [destination, setDestination] = useState({ lat: 51.509372, lng: -0.127758 });
+
   return (
     <>
-      <GoogleMap />
+      <StyledMainContainer>
+        <StyledMapWrapper>
+          <GoogleMap origin={origin} destination={destination} />
+        </StyledMapWrapper>
+        <StyledLocation>
+          <AutoLocation setPosition={setOrigin}></AutoLocation>
+          <AutoLocation setPosition={setDestination}></AutoLocation>
+          <Button type="primary">라이더 탐색</Button>
+        </StyledLocation>
+      </StyledMainContainer>
     </>
   );
 };
