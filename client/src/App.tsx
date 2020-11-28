b8fbf374357459943f6fa648cd3d74b1a2de2437
hoo00nn <hoo00nn@naver.com>
2020-11-28 20:15:40 +0900

    Feat #72 : ChatRoom 페이지 라우팅 설정
    - 채팅방 페이지 라우팅 설정

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 
+import ChatRoom from '@routes/ChatRoom';
 import SignIn from '@/routes/SignIn';
 import Home from '@routes/Home';
 import Signup from '@routes/Signup';
@@ -28,6 +29,8 @@ const App: FC = () => (
             <Route exact path="/" component={auth(Home)} />
             <Route exact path="/signin" component={SignIn} />
             <Route exact path="/signup" component={Signup} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/chatroom" component={auth(ChatRoom)} />
           </Switch>
         </BrowserRouter>
       </ThemeProvider>
