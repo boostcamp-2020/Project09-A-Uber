d572271a0985ac39fdc35ff3218f6719155de6d1
kimhakjun <dkd7142@naver.com>
2020-11-29 14:20:52 +0900

    Feat #80 : inValidate Message 적용

--- client/src/utils/client-message.ts
+++ client/src/utils/client-message.ts
@@ -4,6 +4,7 @@ export const Message = {
   NameGuidance: '2~5 길이의 한글입니다.',
   PasswordGuidance: '숫자 문자 특수문자 조합 8자이상입니다.',
   PhoneGuidance: ' -(하이픈)을 포함하여 작성해주세요.',
-  LicenseGuidance: '입력예시 12-12-123456-12 입니다.',
-  CarNumberGuidance: '입력예시 12가 1234',
+  LicenseGuidance: '입력예시: 12-12-123456-12',
+  CarNumberGuidance: '입력예시: 12가 1234',
+  ExpiryDateGuidance: 'MM/YY',
 };
