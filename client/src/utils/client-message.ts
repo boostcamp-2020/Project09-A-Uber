81fb3f26dd11ef323d409a624fadbd9d66af2e96
kimhakjun <dkd7142@naver.com>
2020-11-29 13:57:48 +0900

    Feat #80 : 클라이언트 메시지 작성
    - 유효하지않은 입력형식일때 보여주기 위한 메시지 작성

--- client/src/utils/client-message.ts
+++ client/src/utils/client-message.ts
@@ -1,4 +1,9 @@
 export const Message = {
   SucceedSignup: '회원가입에 성공하였습니다.',
   SucceedSignin: '로그인에 성공하였습니다.',
+  NameGuidance: '2~5 길이의 한글입니다.',
+  PasswordGuidance: '숫자 문자 특수문자 조합 8자이상입니다.',
+  PhoneGuidance: ' -(하이픈)을 포함하여 작성해주세요.',
+  LicenseGuidance: '입력예시 12-12-123456-12 입니다.',
+  CarNumberGuidance: '입력예시 12가 1234',
 };
