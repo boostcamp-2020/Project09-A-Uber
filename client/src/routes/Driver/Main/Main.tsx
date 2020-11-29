84e5487cdb6a4e52852c632609552049dd7d8dad
chanyeong <cyjo9603@naver.com>
2020-11-29 21:10:18 +0900

    Feat #82 : 오더 리스트 드라이버 페이지에 표시
    - 오더 리스트 드라이버 페이지에 표시

--- client/src/routes/Driver/Main/Main.tsx
+++ client/src/routes/Driver/Main/Main.tsx
@@ -1,9 +1,34 @@
+/* eslint-disable no-underscore-dangle */
 import React, { FC } from 'react';
+import { useCustomQuery } from '@hooks/useApollo';
 
+import styled from '@theme/styled';
 import MapFrame from '@components/MapFrame';
+import OrderLog from '@components/OrderLog';
+import { GET_UNASSIGNED_ORDERS } from '@queries/order.queries';
+import { GetUnassignedOrders } from '@/types/api';
+
+const StyledOrderLogList = styled.section`
+  height: 100%;
+  overflow: scroll;
+
+  & > div {
+    margin-bottom: 0.5rem;
+  }
+`;
 
 const Main: FC = () => {
-  return <MapFrame></MapFrame>;
+  const { data: orders } = useCustomQuery<GetUnassignedOrders>(GET_UNASSIGNED_ORDERS);
+
+  return (
+    <MapFrame>
+      <StyledOrderLogList>
+        {orders?.getUnassignedOrders.unassignedOrders?.map((order) => (
+          <OrderLog order={order} key={`order_list_${order._id}`} />
+        ))}
+      </StyledOrderLogList>
+    </MapFrame>
+  );
 };
 
 export default Main;
