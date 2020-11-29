f5f735e2643ddffa7e133ffc0600c6d8231db264
kimhakjun <dkd7142@naver.com>
2020-11-29 22:45:43 +0900

    Feat #78 : 위치 구독 subscription 구현
    - subscription 의 파라미터로 orderId 전달하여 사용자가 자신의 order 의 운전자의 위치를 구독할 수 있게 구현

---
+++ server/src/api/user/subLocation/subLocation.resolvers.ts
@@ -0,0 +1,18 @@
+import { Resolvers } from '@type/api';
+import { withFilter } from 'apollo-server-express';
+import { DIRVER_UPADTE } from '@api/user/updateUserLocation/updateLocation.resolvers';
+
+const resolvers: Resolvers = {
+  Subscription: {
+    subLocation: {
+      subscribe: withFilter(
+        (_, __, { pubsub }) => pubsub.asyncIterator(DIRVER_UPADTE),
+        (payload, variables) => {
+          return payload.subLocation.orderId === variables.orderId;
+        },
+      ),
+    },
+  },
+};
+
+export default resolvers;
