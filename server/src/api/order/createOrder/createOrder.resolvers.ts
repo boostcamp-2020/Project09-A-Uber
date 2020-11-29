5ddbc7c92cf3a934d99f5703d808bf4214cada9c
kimhakjun <dkd7142@naver.com>
2020-11-30 00:37:43 +0900

    Feat #78 : 코드 수정
    - req.user.id 사용하도록 변경
    
    - 구독 이벤트 매직넘버 삭제 변수로 변경

--- server/src/api/order/createOrder/createOrder.resolvers.ts
+++ server/src/api/order/createOrder/createOrder.resolvers.ts
@@ -7,7 +7,7 @@ const resolvers: Resolvers = {
       const { result, orderId, error } = await createOrder({
         startingPoint,
         destination,
-        user: req.user || '',
+        user: req.user?._id || '',
       });
 
       if (result === 'fail' || error) {
