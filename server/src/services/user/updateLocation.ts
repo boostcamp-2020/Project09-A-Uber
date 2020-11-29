93ef64a712d004ccb03cc952ed5fb7f413c0c0cd
kimhakjun <dkd7142@naver.com>
2020-11-30 00:40:30 +0900

    Feat #78 : 위치갱신 mutation 작성
    - 위치 갱신 mutation 생성, 현재 사용자가 드라이버고 현재 매칭중인 order 가있다면 현재위치 orderid 와 함께 publish

---
+++ server/src/services/user/updateLocation.ts
@@ -0,0 +1,23 @@
+import User from '@models/user';
+import { Location } from '@models/location';
+
+interface UpdateLocationProps {
+  userId?: string;
+  curLocation: Location;
+}
+
+const updateCurLocation = async ({ userId, curLocation }: UpdateLocationProps) => {
+  try {
+    await User.updateOne(
+      { _id: userId },
+      {
+        location: curLocation,
+      },
+    );
+    return { result: 'success' };
+  } catch (err) {
+    return { result: 'fail', error: err.message };
+  }
+};
+
+export default updateCurLocation;
