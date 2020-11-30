ad5db370ea0efb16276631a4edc7eead3ff12f85
hoo00nn <hoo00nn@naver.com>
2020-11-30 23:20:20 +0900

    Feat #93 : 오더 승인 service 로직 추가 구현
    - 오더 승인 시 새로운 오더 리스트 목록을 가져와 같이 반환하도록 구현

--- server/src/services/order/approvalOrder.ts
+++ server/src/services/order/approvalOrder.ts
@@ -1,10 +1,12 @@
 import Order from '@models/order';
+import getUnassignedOrders from '@services/order/getUnassingedOrders';
 
 const approvalOrder = async (driverId: string, orderId: string) => {
   try {
     await Order.findByIdAndUpdate(orderId, { driver: driverId, status: 'active' });
+    const { unassignedOrders } = await getUnassignedOrders(driverId || '');
 
-    return { result: 'success' };
+    return { result: 'success', unassignedOrders };
   } catch (err) {
     return { result: 'fail', error: err.message };
   }
