a055555801f91676f8e73beac971c84d2c0b5772
kimhakjun <dkd7142@naver.com>
2020-11-30 15:20:17 +0900

    Feat #94 : 드라이버 관련 enums 추가
    - 푸시알람을 띄어주기위해 드라이버의 인접거리를 나타내는 emuns 추가

--- client/src/utils/enums.ts
+++ client/src/utils/enums.ts
@@ -1,8 +1,13 @@
-// eslint-disable-next-line no-shadow
+/* eslint-disable no-shadow */
+
 export enum TOAST_DURATION {
   SIGNUP_SUCCESS = 1,
   SIGNUP_FAILURE = 3,
 
   SIGNIN_SUCCESS = 0.5,
   SIGNIN_FAILURE = 2,
+}
+
+export enum DRIVER {
+  ABJACENT_DISTANCE = 100,
 }
