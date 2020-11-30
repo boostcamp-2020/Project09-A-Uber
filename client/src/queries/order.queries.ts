4768c98e950c735b34f67023e261d1193811cb43
jongeunk0613 <gladiolus4231@gmail.com>
2020-11-30 18:10:06 +0900

    Feat #84 : 오더 정보 조회 query 작성
    - 오더 정보를 조회하는 query 작성

--- client/src/queries/order.queries.ts
+++ client/src/queries/order.queries.ts
@@ -10,6 +10,26 @@ export const CREATE_ORDER = gql`
   }
 `;
 
+export const GET_ORDER = gql`
+  query GetOrderInfo($orderId: String!) {
+    getOrderInfo(orderId: $orderId) {
+      result
+      order {
+        _id
+        startingPoint {
+          address
+          coordinates
+        }
+        destination {
+          address
+          coordinates
+        }
+      }
+      error
+    }
+  }
+`;
+
 export const GET_UNASSIGNED_ORDERS = gql`
   query GetUnassignedOrders {
     getUnassignedOrders {
