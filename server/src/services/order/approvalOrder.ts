e3b4362b43430f1facaf0de8bdead0b41f95c41c
hoo00nn <hoo00nn@naver.com>
2020-12-01 01:12:13 +0900

    Refactor #93 : 오더 승인 시 오더 리스트 조회 쿼리 삭제
    - subscription 성공 시 클라이언트 쪽에서 다시 쿼리를 날려 리스트를 가져오는 형태로 구현하기 때문에 서버쪽에 불필요한 조회는 필요없다고 생각하여 삭제

--- server/src/services/order/approvalOrder.ts
+++ server/src/services/order/approvalOrder.ts
@@ -1,12 +1,10 @@
 import Order from '@models/order';
-import getUnassignedOrders from '@services/order/getUnassingedOrders';
 
 const approvalOrder = async (driverId: string, orderId: string) => {
   try {
     await Order.findByIdAndUpdate(orderId, { driver: driverId, status: 'active' });
-    const { unassignedOrders } = await getUnassignedOrders(driverId || '');
 
-    return { result: 'success', unassignedOrders };
+    return { result: 'success' };
   } catch (err) {
     return { result: 'fail', error: err.message };
   }
