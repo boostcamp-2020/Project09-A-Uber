71d422c5d3e36abcca4cc381486c64390e4a85cb
김종은 <jongeunkim@gimjong-eun-ui-MacBookPro.local>
2020-11-30 15:28:59 +0900

    Feat #84 : 입력한 id의 오더 정보를 불러오는 service 구현
    - 입력한 Id의 오더 정보를 반환한다

---
+++ server/src/services/order/getOrder.ts
@@ -0,0 +1,24 @@
+import { Order as OrderType } from '@type/api';
+import Order from '@models/order';
+import { Message } from '@util/server-message';
+
+interface GetOrderProps {
+  orderId: string;
+  driverId: string;
+}
+
+const getOrder = async ({ orderId, driverId }: GetOrderProps) => {
+  try {
+    // const order = (await Order.findOne({ _id: orderId, user: userId })) as OrderType | null;
+    const order = (await Order.findOne({ _id: orderId })) as OrderType | null;
+
+    if (!order) {
+      return { result: 'fail', order: null, error: Message.OrderNotFound };
+    }
+    return { result: 'success', order };
+  } catch (err) {
+    return { result: 'fail', order: null, error: err.message };
+  }
+};
+
+export default getOrder;
