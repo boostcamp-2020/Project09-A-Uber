52226af631cb150a9870393a063f9eac6c578ad3
hoo00nn <hoo00nn@naver.com>
2020-12-01 03:41:10 +0900

    Feat #108 : getChat 서비스 함수 구현
    - orderID를 받아서 해당 orderId의 chat 리스트 가져오도록 구현

---
+++ server/src/services/chat/getChat.ts
@@ -0,0 +1,13 @@
+import OrderModel from '@models/order';
+
+const getChatList = async (orderId: string) => {
+  try {
+    const chat = await OrderModel.findById(orderId, 'chat');
+
+    return { result: 'success', chat: chat?.get('chat') };
+  } catch (err) {
+    return { result: 'fail', error: err.message };
+  }
+};
+
+export default getChatList;
