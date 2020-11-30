21ed371d9552f00577b08494b6c934349cc564c4
hoo00nn <hoo00nn@naver.com>
2020-11-30 23:21:20 +0900

    Feat #93 : 오더 승인 시 오더 리스트 업데이트 구독 이벤트 발생하도록 구현
    - 오더 승인 시 새로운 오더리스트로 업데이트 되야 하기 때문에 오더 승인이 발생할 때 마다 오더 리스트 새로 가져오도록 구현

--- server/src/api/order/approvalOrder/approvalOrder.resolvers.ts
+++ server/src/api/order/approvalOrder/approvalOrder.resolvers.ts
@@ -1,18 +1,22 @@
 import { Resolvers } from '@type/api';
 import approvalOrder from '@services/order/approvalOrder';
+import { UPDATE_ORDER_LIST } from '@api/order/updateOrderList/updateOrderList.resolvers';
 
 export const APPROVAL_ORDER = 'APPROVAL_ORDER';
 
 const resolvers: Resolvers = {
   Mutation: {
     approvalOrder: async (_, { orderId }, { req, pubsub }) => {
-      const { result, error } = await approvalOrder(req.user?._id || '', orderId);
+      const { result, error, unassignedOrders } = await approvalOrder(req.user?._id || '', orderId);
 
       if (result === 'fail' || error) {
         return { result, error };
       }
 
       pubsub.publish(APPROVAL_ORDER, { subApprovalOrder: { approvalOrderId: orderId } });
+      pubsub.publish(UPDATE_ORDER_LIST, {
+        updateOrderList: { unassignedOrders, result: 'success' },
+      });
 
       return { result };
     },
