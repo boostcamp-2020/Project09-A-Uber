2099374a8bb1cbf6736287f69de2c69ed0e8e32f
kimhakjun <dkd7142@naver.com>
2020-12-01 01:46:36 +0900

    Feat #102 : 운행시작시 현재위치 목적지 위치 경로 표시
    - 첫렌더링시 order 정보 가져와서 목적지 설정
    - callquery 를 이용하여 첫렌더링에만 order 정보 요청하도록 구현

--- client/src/routes/Driver/GoToDestination/GoToDestination.tsx
+++ client/src/routes/Driver/GoToDestination/GoToDestination.tsx
@@ -1,10 +1,11 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import MapFrame from '@/components/MapFrame';
 import { GET_ORDER } from '@queries/order.queries';
 import { GetOrderInfo } from '@/types/api';
 import { useCustomQuery } from '@hooks/useApollo';
 import { Button } from 'antd-mobile';
 import styled from '@/theme/styled';
+import getUserLocation from '@utils/getUserLocation';
 
 const StyledGoToDestinationMenu = styled.div`
   display: flex;
@@ -34,8 +35,39 @@ const StyledGoToDestinationMenu = styled.div`
 `;
 
 const GoToDestination: FC = () => {
+  const [directions, setDirections] = useState<google.maps.DirectionsResult | undefined>(undefined);
+  const [currentLocation, setCurrentLocation] = useState({ lat: 0, lng: 0 });
+  const [destination, setDestination] = useState({ lat: 0, lng: 0 });
+  const { callQuery } = useCustomQuery<GetOrderInfo>(GET_ORDER, { skip: true });
+
+  const updateCurrentLocation = async () => {
+    const currLocation = await getUserLocation();
+
+    if (currLocation) {
+      setCurrentLocation(currLocation);
+    }
+  };
+
+  useEffect(() => {
+    (async () => {
+      const result = await callQuery({ orderId: '5fc39490270c0a452ebfde4a' });
+      const lat = result?.data.getOrderInfo.order?.destination.coordinates[0] as number;
+      const lng = result?.data.getOrderInfo.order?.destination.coordinates[1] as number;
+      setDestination({ lat, lng });
+    })();
+    const updateCurrentLocationInterval = setInterval(updateCurrentLocation, 5000);
+    return () => {
+      clearInterval(updateCurrentLocationInterval);
+    };
+  }, []);
+
   return (
-    <MapFrame>
+    <MapFrame
+      origin={currentLocation}
+      destination={destination}
+      setDirections={setDirections}
+      directions={directions}
+    >
       <StyledGoToDestinationMenu>
         <span>요금부분: TODO</span>
         <Button className="driver-chat-btn">손님과의 채팅</Button>
