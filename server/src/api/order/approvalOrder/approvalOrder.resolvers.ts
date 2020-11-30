4679b442dc6656e818064c84d0eaa87d5e0cdcc0
chanyeong <cyjo9603@naver.com>
2020-11-30 15:47:12 +0900

    Feat #91: 오더 승인 Subscription resolver 구현
    - 오더 승인되는 것을 구독 할 구현
    - 오더 승인 시 publish로 승인 호출

--- server/src/api/order/approvalOrder/approvalOrder.resolvers.ts
+++ server/src/api/order/approvalOrder/approvalOrder.resolvers.ts
@@ -1,14 +1,18 @@
 import { Resolvers } from '@type/api';
 import approvalOrder from '@services/order/approvalOrder';
 
+export const APPROVAL_ORDER = 'APPROVAL_ORDER';
+
 const resolvers: Resolvers = {
   Mutation: {
-    approvalOrder: async (_, { orderId }, { req }) => {
+    approvalOrder: async (_, { orderId }, { req, pubsub }) => {
       const { result, error } = await approvalOrder(req.user?._id || '', orderId);
 
       if (result === 'fail' || error) {
         return { result, error };
       }
+
+      pubsub.publish(APPROVAL_ORDER, { subApprovalOrder: { approvalOrderId: orderId } });
 
       return { result };
     },
