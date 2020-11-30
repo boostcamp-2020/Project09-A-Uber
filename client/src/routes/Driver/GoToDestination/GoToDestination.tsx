fc5424076935e5fbbbae2e2d3400d3afe24b5e5c
kimhakjun <dkd7142@naver.com>
2020-12-01 00:29:19 +0900

    Feat #102 : 드라이버 목적지 페이지 생성
    - 운행시작을 누르면 보여지는 goToDestination 페이지 구현

--- client/src/routes/Driver/GoToDestination/GoToDestination.tsx
+++ client/src/routes/Driver/GoToDestination/GoToDestination.tsx
@@ -1,7 +1,50 @@
-import React from 'react';
+import React, { FC } from 'react';
+import MapFrame from '@/components/MapFrame';
+import { GET_ORDER } from '@queries/order.queries';
+import { GetOrderInfo } from '@/types/api';
+import { useCustomQuery } from '@hooks/useApollo';
+import { Button } from 'antd-mobile';
+import styled from '@/theme/styled';
 
-const GoToDestination = () => {
-  return <>hihi 출발 페이지야!</>;
+const StyledGoToDestinationMenu = styled.div`
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  justify-content: flex-end;
+
+  & span {
+    text-align: center;
+  }
+
+  & .am-button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 1rem;
+    height: 2rem;
+    cursor: pointer;
+    font-weight: 700;
+    font-size: 0.9rem;
+  }
+
+  & .driver-chat-btn {
+    color: ${({ theme }) => theme.PRIMARY};
+    border: 1px solid ${({ theme }) => theme.PRIMARY};
+  }
+`;
+
+const GoToDestination: FC = () => {
+  return (
+    <MapFrame>
+      <StyledGoToDestinationMenu>
+        <span>요금부분: TODO</span>
+        <Button className="driver-chat-btn">손님과의 채팅</Button>
+        <Button className="driver-arrive-btn" type="primary">
+          도착완료
+        </Button>
+      </StyledGoToDestinationMenu>
+    </MapFrame>
+  );
 };
 
 export default GoToDestination;
