d0f9765b1997ee96f3a1e298c52a6045366a861f
jongeunk0613 <gladiolus4231@gmail.com>
2020-11-30 18:11:25 +0900

    Feat #84 : 탑승지까지 이동하는 경로 보여주는 페이지 구현
    - 지도의 도착지를 오더의 출발지로 설정
    - 지도의 출발지를 현재 위치로 설정
    - 현재 위치를 5초마다 update하도록 구현

---
+++ client/src/routes/Driver/GoToOrigin/GoToOrigin.tsx
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from 'react';
+import { Button } from 'antd-mobile';
+import styled from '@theme/styled';
+
+import MapFrame from '@components/MapFrame';
+import { GET_ORDER } from '@queries/order.queries';
+import { GetOrderInfo } from '@/types/api';
+import { Location } from '@components/GoogleMap/GoogleMap';
+import { useCustomQuery } from '@hooks/useApollo';
+
+const StyledDriverGoToOriginMenu = styled.section`
+  height: 100%;
+  display: flex;
+  align-items: flex-end;
+
+  & > .driver-start-order-btn {
+    width: 100%;
+    & > .driver-start-order-info {
+      display: flex;
+      justify-content: center;
+      color: grey;
+    }
+
+    & > .am-button {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      cursor: pointer;
+      height: 2rem;
+      margin: 0.8rem 0;
+      font-weight: 700;
+      font-size: 0.9rem;
+    }
+  }
+`;
+
+const GoToOrigin = () => {
+  const [directions, setDirections] = useState<google.maps.DirectionsResult | undefined>(undefined);
+  const [currentLocation, setCurrentLocation] = useState({ lat: 0, lng: 0 });
+  const { data } = useCustomQuery<GetOrderInfo>(GET_ORDER, {
+    variables: { orderId: '5fc45539e439ea40e869bf47' },
+  });
+
+  const order = data?.getOrderInfo.order;
+
+  const updateCurrentLocation = async () => {
+    const currLocation = await new Promise<Location>((res) => {
+      window.navigator.geolocation.getCurrentPosition((position) => {
+        res({ lat: position.coords.latitude, lng: position.coords.longitude });
+      });
+    });
+    setCurrentLocation(currLocation);
+  };
+
+  useEffect(() => {
+    const updateCurrentLocationInterval = setInterval(updateCurrentLocation, 5000);
+
+    return () => {
+      clearInterval();
+    };
+  }, []);
+
+  return (
+    <>
+      <MapFrame
+        origin={currentLocation}
+        destination={{
+          lat: order?.startingPoint.coordinates[0] as number,
+          lng: order?.startingPoint.coordinates[1] as number,
+        }}
+        setDirections={setDirections}
+        directions={directions}
+      >
+        <StyledDriverGoToOriginMenu>
+          <div className="driver-start-order-btn">
+            <div className="driver-start-order-info">손님이 탑승하시고 나서 눌러주세요.</div>
+            <Button type="primary">운행시작</Button>
+          </div>
+        </StyledDriverGoToOriginMenu>
+      </MapFrame>
+    </>
+  );
+};
+
+export default GoToOrigin;
