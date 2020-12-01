70d080269a3f471b2f2073e658114218330434b2
jongeunk0613 <gladiolus4231@gmail.com>
2020-12-01 16:59:38 +0900

    Feat #120 : 드라이버가 출발지 근처에 다가왔을 때 탑승자에게 팝업 표시
    - 드라이버가 출발지 근방 100m 안에 다가왔을 때 탑승자에게 드라이버 정보와 함께 팝업을 띄움

--- client/src/routes/User/WaitingDriver/WaitingDriver.tsx
+++ client/src/routes/User/WaitingDriver/WaitingDriver.tsx
@@ -1,13 +1,23 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { Button } from 'antd-mobile';
 import styled from '@theme/styled';
 import { useSubscription } from '@apollo/react-hooks';
 
 import MapFrame from '@components/MapFrame';
-import { GET_ORDER } from '@queries/order.queries';
+import { GET_ORDER, GET_ORDER_CAR_INFO } from '@queries/order.queries';
 import { SUB_DRIVER_LOCATION } from '@queries/user.queries';
-import { GetOrderInfo, SubDriverLocation } from '@/types/api';
+import {
+  GetOrderInfo,
+  SubDriverLocation,
+  GetOrderCarInfo,
+  CarInfo as CarInfoType,
+} from '@/types/api';
 import { useCustomQuery } from '@hooks/useApollo';
+import { calcLocationDistance } from '@utils/calcLocationDistance';
+import CarInfo from '@components/CarInfo';
+import Modal from '@components/Modal';
+import useModal from '@hooks/useModal';
+import { Message } from '@utils/client-message';
 
 const StyledWaitingDriverMenu = styled.section`
   height: 100%;
@@ -33,34 +43,56 @@ const StyledWaitingDriverMenu = styled.section`
 const WaitingDriver = () => {
   const [directions, setDirections] = useState<google.maps.DirectionsResult | undefined>(undefined);
   const [driverLocation, setDriverLocation] = useState({ lat: 0, lng: 0 });
+  const [isModal, openModal, closeModal] = useModal();
+  const [carInfo, setCarInfo] = useState({ carNumber: '', carType: 'small' } as CarInfoType);
+  useCustomQuery<GetOrderCarInfo>(GET_ORDER_CAR_INFO, {
+    variables: { orderId: '5fc45539e439ea40e869bf47' },
+    onCompleted: (data) => {
+      setCarInfo({
+        carNumber: data.getOrderCarInfo.carInfo?.carNumber,
+        carType: data.getOrderCarInfo.carInfo?.carType,
+      } as CarInfoType);
+    },
+  });
   const { data } = useCustomQuery<GetOrderInfo>(GET_ORDER, {
     variables: { orderId: '5fc45539e439ea40e869bf47' },
   });
+  const destination = {
+    lat: data?.getOrderInfo.order?.startingPoint.coordinates[0] as number,
+    lng: data?.getOrderInfo.order?.destination.coordinates[1] as number,
+  };
   useSubscription<SubDriverLocation>(SUB_DRIVER_LOCATION, {
     variables: { orderId: '5fc45539e439ea40e869bf47' },
     onSubscriptionData: ({ subscriptionData }) => {
-      const driverNewLocation = subscriptionData.data?.subDriverLocation.coordinates as number[];
-      setDriverLocation({ lat: driverNewLocation[0], lng: driverNewLocation[1] });
+      const driverNewLocation = {
+        lat: subscriptionData.data?.subDriverLocation.coordinates[0] as number,
+        lng: subscriptionData.data?.subDriverLocation.coordinates[1] as number,
+      };
+      setDriverLocation(driverNewLocation);
+      if (calcLocationDistance(driverNewLocation, destination) < 100) {
+        openModal();
+      }
     },
   });
-  const order = data?.getOrderInfo.order;
 
   return (
-    <MapFrame
-      origin={driverLocation}
-      destination={{
-        lat: order?.startingPoint.coordinates[0] as number,
-        lng: order?.startingPoint.coordinates[1] as number,
-      }}
-      setDirections={setDirections}
-      directions={directions}
-    >
-      <StyledWaitingDriverMenu>
-        <div className="cancel-order-btn">
-          <Button type="warning">취소</Button>
-        </div>
-      </StyledWaitingDriverMenu>
-    </MapFrame>
+    <>
+      <MapFrame
+        origin={driverLocation}
+        destination={destination}
+        setDirections={setDirections}
+        directions={directions}
+      >
+        <StyledWaitingDriverMenu>
+          <div className="cancel-order-btn">
+            <Button type="warning">취소</Button>
+          </div>
+        </StyledWaitingDriverMenu>
+      </MapFrame>
+      <Modal visible={isModal} onClose={closeModal}>
+        {carInfo && <CarInfo carInfo={carInfo} title={Message.DriverAjacent} />}
+      </Modal>
+    </>
   );
 };
 
