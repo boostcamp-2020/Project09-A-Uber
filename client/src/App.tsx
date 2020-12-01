bb8d17774d2fbd5ba1d3930d4f1946c41c42faef
hoo00nn <hoo00nn@naver.com>
2020-12-01 13:52:22 +0900

    Feat #108 : 채팅방 페이지 라우팅 설정
    - /chatroom/:chatId 로 구성 chatId는 order번호

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -38,7 +38,7 @@ const App: FC = () => (
             <Route exact path="/user/searchDriver" component={auth(SearchDriver, 'user')} />
             <Route exact path="/signin" component={SignIn} />
             <Route exact path="/signup" component={Signup} />
-            <Route exact path="/chatroom" component={auth(ChatRoom)} />
+            <Route exact path="/chatroom/:chatId" component={auth(ChatRoom)} />
           </Switch>
         </BrowserRouter>
       </ThemeProvider>
