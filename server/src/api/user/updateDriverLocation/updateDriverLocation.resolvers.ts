39f41db8a310e02cc54ae2a74ce2b1ffb156186e
kimhakjun <dkd7142@naver.com>
2020-12-02 00:04:14 +0900

    Refactor #130 : 위치갱신 리졸버 수정
    - 드라이버의 위치 갱신중 매칭중인 order 있을때만 publish
    - 매칭중인 오더조회 결과값에따라 에러처리

--- server/src/api/user/updateDriverLocation/updateDriverLocation.resolvers.ts
+++ server/src/api/user/updateDriverLocation/updateDriverLocation.resolvers.ts
@@ -1,4 +1,5 @@
 import { Resolvers } from '@type/api';
+import getActiveDriverOrder from '@services/order/getActiveDriverOrder';
 import updateDriverLocation from '@services/user/updateDriverLocation';
 
 export const UPDATE_DRIVER_LOCATION = 'UPDATE_DRIVER_LOCATION';
@@ -6,17 +7,26 @@ export const UPDATE_DRIVER_LOCATION = 'UPDATE_DRIVER_LOCATION';
 const resolvers: Resolvers = {
   Mutation: {
     updateDriverLocation: async (_, { lat, lng }, { req, pubsub }) => {
-      const { result, orderId, error } = await updateDriverLocation({
+      const { result, error } = await updateDriverLocation({
         userId: req.user?._id,
         userType: req.user?.type,
         curLocation: { coordinates: [lat, lng] },
       });
-      pubsub.publish(UPDATE_DRIVER_LOCATION, {
-        subDriverLocation: { coordinates: [lat, lng], orderId },
-      });
 
       if (result === 'fail') {
         return { result, error };
+      }
+
+      const { inquiryResult, order, inquiryError } = await getActiveDriverOrder(
+        req.user?._id || '',
+      );
+      if (inquiryResult === 'success' && order) {
+        pubsub.publish(UPDATE_DRIVER_LOCATION, {
+          subDriverLocation: { coordinates: [lat, lng], orderId: order._id },
+        });
+      }
+      if (inquiryResult === 'fail') {
+        return { result: inquiryResult, error: inquiryError };
       }
 
       return { result };
