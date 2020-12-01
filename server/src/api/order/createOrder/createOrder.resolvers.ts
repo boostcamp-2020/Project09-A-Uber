bdebdcb67bb4fea3329e72795813e11a3fe06488
hoo00nn <hoo00nn@naver.com>
2020-12-01 17:18:32 +0900

    Feat #107 : 오더 생성 시 리스트 자동업데이트 구현

--- server/src/api/order/createOrder/createOrder.resolvers.ts
+++ server/src/api/order/createOrder/createOrder.resolvers.ts
@@ -1,9 +1,11 @@
 import { Resolvers } from '@type/api';
 import createOrder from '@services/order/createOrder';
 
+import { UPDATE_ORDER_LIST } from '@api/order/updateOrderList/updateOrderList.resolvers';
+
 const resolvers: Resolvers = {
   Mutation: {
-    createOrder: async (_, { startingPoint, destination }, { req }) => {
+    createOrder: async (_, { startingPoint, destination }, { req, pubsub }) => {
       const { result, orderId, error } = await createOrder({
         startingPoint,
         destination,
@@ -13,6 +15,8 @@ const resolvers: Resolvers = {
       if (result === 'fail' || error) {
         return { result, error };
       }
+
+      pubsub.publish(UPDATE_ORDER_LIST, { updateOrderList: { result: 'success' } });
 
       return { result, orderId };
     },
