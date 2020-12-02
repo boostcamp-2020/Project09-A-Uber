8adfb245efdae00a53189116f16adb84311512bb
chanyeong <cyjo9603@naver.com>
2020-12-02 21:47:13 +0900

    Feat #144 : UserGoToDestination 페이지 기본 레이아웃 구성
    - UserGoToDestination 페이지 기본 레이아웃 구성
    - 기존의 waitingDriver 페이지와 거의 유사해 스타일을 그대로 가져와 사용함
      - 추후 이부분은 리팩터링이 필요함

--- client/src/routes/User/GoToDestination/GoToDestination.tsx
+++ client/src/routes/User/GoToDestination/GoToDestination.tsx
@@ -1,5 +1,55 @@
-import React, { FC } from 'react';
+import React, { FC, useCallback } from 'react';
+import { Button } from 'antd-mobile';
+import styled from '@theme/styled';
+import { useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
-const GoToDestination: FC = () => <></>;
+import MapFrame from '@components/MapFrame';
+import { InitialState } from '@reducers/.';
+
+// TODO: user/waitingDriver의 스타일과 유사, 추후 리팩터링 필요
+const StyledUserGoToDestinationMenu = styled.section`
+  height: 100%;
+  display: flex;
+  align-items: flex-end;
+
+  & > .chat-with-driver {
+    width: 100%;
+
+    & > .am-button {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      cursor: pointer;
+      height: 2rem;
+      margin: 0.8rem 0;
+      font-weight: 700;
+      font-size: 0.9rem;
+    }
+  }
+`;
+
+const GoToDestination: FC = () => {
+  const history = useHistory();
+  const { id: orderId } = useSelector((state: InitialState) => state.order || {});
+
+  const onClickChatRoom = useCallback(() => {
+    history.push(`/chatroom/${orderId}`);
+  }, []);
+
+  return (
+    <>
+      <MapFrame>
+        <StyledUserGoToDestinationMenu>
+          <div className="chat-with-driver">
+            <Button type="primary" onClick={onClickChatRoom}>
+              드라이버와 채팅하기
+            </Button>
+          </div>
+        </StyledUserGoToDestinationMenu>
+      </MapFrame>
+    </>
+  );
+};
 
 export default GoToDestination;
