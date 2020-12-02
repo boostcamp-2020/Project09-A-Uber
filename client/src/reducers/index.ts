0fa5f5fef5d6a038247195f7ec4377242b2848dc
chanyeong <cyjo9603@naver.com>
2020-12-02 22:24:06 +0900

    Refactor #145 : location 업데이트 reducer 작성
    - origin과 destination의 location을 업데이트할 reducer 작성

--- client/src/reducers/index.ts
+++ client/src/reducers/index.ts
@@ -1,5 +1,6 @@
 import { CarInfo } from '@/types/api';
 import { OrderActions, ADD_ORDER_ID, ADD_CAR_INFO } from './order';
+import { LocationActions, UPDATE_LOCATION_ORIGIN, UPDATE_LOCATION_DESTINATION } from './location';
 
 interface Driver {
   licenseNumber: string;
@@ -31,14 +32,22 @@ export interface InitialState {
 
 const initialState: InitialState = { location: { isFixCenter: false } };
 
-type Action = OrderActions;
+type Action = OrderActions | LocationActions;
 
 const reducer = (state: InitialState = initialState, action: Action): InitialState => {
   switch (action.type) {
+    // order
     case ADD_ORDER_ID:
       return { ...state, order: { ...state.order, id: action.orderId } };
     case ADD_CAR_INFO:
       return { ...state, order: { ...state.order, carInfo: action.carInfo } };
+
+    // location
+    case UPDATE_LOCATION_ORIGIN:
+      return { ...state, location: { ...state.location, origin: action.location } };
+    case UPDATE_LOCATION_DESTINATION:
+      return { ...state, location: { ...state.location, destination: action.location } };
+
     default:
       return state;
   }
