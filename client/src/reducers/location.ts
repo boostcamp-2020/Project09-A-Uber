7a7004c524a5ed57e5bd654bc29601643f7537e2
chanyeong <cyjo9603@naver.com>
2020-12-02 22:40:23 +0900

    Refactor #145 : location 업데이트 시 반값 업데이트 가능
    - location 업데이트 시 반값 업데이트 가능 (삭제 기능)

--- client/src/reducers/location.ts
+++ client/src/reducers/location.ts
@@ -7,10 +7,10 @@ export const UPDATE_LOCATION_ALL = 'UPDATE_LOCATION_ALL';
 // updateLocationOrigin
 export interface UpdateLocationOrigin {
   type: typeof UPDATE_LOCATION_ORIGIN;
-  location: Location;
+  location?: Location;
 }
 
-export const updateLocationOrigin = (location: Location): UpdateLocationOrigin => ({
+export const updateLocationOrigin = (location?: Location): UpdateLocationOrigin => ({
   type: UPDATE_LOCATION_ORIGIN,
   location,
 });
@@ -18,22 +18,30 @@ export const updateLocationOrigin = (location: Location): UpdateLocationOrigin =
 // updateLocationDestination
 export interface UpdateLocationDestination {
   type: typeof UPDATE_LOCATION_DESTINATION;
-  location: Location;
+  location?: Location;
 }
 
-export const updateLocationDestination = (location: Location): UpdateLocationDestination => ({
+export const updateLocationDestination = (location?: Location): UpdateLocationDestination => ({
   type: UPDATE_LOCATION_DESTINATION,
   location,
 });
 
 // updateLocationAll
-export interface UpdateLocationAll {
-  type: typeof UPDATE_LOCATION_ALL;
-  origin: Location;
-  destination: Location;
+interface UpdateLocationAllProps {
+  origin?: Location;
+  destination?: Location;
 }
 
-export const updateLocationALL = (origin: Location, destination: Location): UpdateLocationAll => ({
+export interface UpdateLocationAll {
+  type: typeof UPDATE_LOCATION_ALL;
+  origin?: Location;
+  destination?: Location;
+}
+
+export const updateLocationALL = ({
+  origin,
+  destination,
+}: UpdateLocationAllProps): UpdateLocationAll => ({
   type: UPDATE_LOCATION_ALL,
   origin,
   destination,
