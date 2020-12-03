00c17e411c017f8bab84f75edf8eb1c93bfe3b54
chanyeong <cyjo9603@naver.com>
2020-12-03 13:55:56 +0900

    Feat #144 : 유저 GoToDestination 페이지 실시간 위치 이동
    - 유저 GoToDestination 페이지 실시간 위치 이동
    - 매 3초마다 유저의 현재 위치가 지도에 표시

--- client/src/routes/User/GoToDestination/GoToDestination.tsx
+++ client/src/routes/User/GoToDestination/GoToDestination.tsx
@@ -1,11 +1,13 @@
-import React, { FC, useCallback } from 'react';
+import React, { FC, useState, useCallback, useEffect } from 'react';
 import { Button } from 'antd-mobile';
 import styled from '@theme/styled';
 import { useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 import MapFrame from '@components/MapFrame';
-import { InitialState } from '@reducers/.';
+import getUserLocation from '@utils/getUserLocation';
+import { USER } from '@utils/enums';
+import { InitialState, Location } from '@reducers/.';
 
 // TODO: user/waitingDriver의 스타일과 유사, 추후 리팩터링 필요
 const StyledUserGoToDestinationMenu = styled.section`
@@ -31,23 +33,52 @@ const StyledUserGoToDestinationMenu = styled.section`
 
 const GoToDestination: FC = () => {
   const history = useHistory();
-  const { id: orderId } = useSelector((state: InitialState) => state.order || {});
+  const { id: orderId } = useSelector(({ order }: InitialState) => order || {});
+  const { destination } = useSelector(({ order }: InitialState) => order.location || {});
+  const [directions, setDirections] = useState<google.maps.DirectionsResult | undefined>(undefined);
+  const [currentLocation, setCurrentLocation] = useState<Location>();
 
   const onClickChatRoom = useCallback(() => {
     history.push(`/chatroom/${orderId}`);
   }, []);
 
+  const updateCurrentLocation = useCallback(async () => {
+    const userLocation = await getUserLocation();
+    if (userLocation) {
+      setCurrentLocation(userLocation);
+    }
+  }, []);
+
+  useEffect(() => {
+    updateCurrentLocation();
+    const updateCurrentLocationInterval = setInterval(
+      updateCurrentLocation,
+      USER.USER_LOCATOIN_UPDATE_TIME,
+    );
+
+    return () => {
+      clearInterval(updateCurrentLocationInterval);
+    };
+  }, []);
+
   return (
     <>
-      <MapFrame>
-        <StyledUserGoToDestinationMenu>
-          <div className="chat-with-driver">
-            <Button type="primary" onClick={onClickChatRoom}>
-              드라이버와 채팅하기
-            </Button>
-          </div>
-        </StyledUserGoToDestinationMenu>
-      </MapFrame>
+      {currentLocation && (
+        <MapFrame
+          origin={currentLocation}
+          destination={destination}
+          directions={directions}
+          setDirections={setDirections}
+        >
+          <StyledUserGoToDestinationMenu>
+            <div className="chat-with-driver">
+              <Button type="primary" onClick={onClickChatRoom}>
+                드라이버와 채팅하기
+              </Button>
+            </div>
+          </StyledUserGoToDestinationMenu>
+        </MapFrame>
+      )}
     </>
   );
 };
