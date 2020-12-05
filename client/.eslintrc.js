28d07106725e4266d44cef83542df57029fb18d3
chanyeong <cyjo9603@naver.com>
2020-12-05 17:18:05 +0900

    Chore #166 : eslint에 prettier 플러그인 적용
    - eslint에 prettier 플러그인 적용

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'prettier'],
   extends: [
     'airbnb-base',
     'plugin:import/errors',
