9585580476e97e18589cf9b7184d5fe50c99bb88
jongeunk0613 <gladiolus4231@gmail.com>
2020-12-05 15:25:20 +0900

    Feat #146 : 운행종료된 오더들을 보여주는 페이지 구현

--- client/src/routes/OrderHistory/OrderHistory.tsx
+++ client/src/routes/OrderHistory/OrderHistory.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 import OrderHistoryList from '@/components/OrderHistoryList';
-import { GetCompletedOrders } from '@/types/api';
+import {
+  GetCompletedOrders,
+  GetCompletedOrders_getCompletedOrders_completedOrders as CompletedOrders,
+} from '@/types/api';
 import { GET_COMPLETED_ORDER } from '@queries/order.queries';
 import { useCustomQuery } from '@hooks/useApollo';
+import HeaderWithBack from '@components/HeaderWithBack';
 
 const OrderHistory = () => {
-  const { data: orders } = useCustomQuery<GetCompletedOrders>(GET_COMPLETED_ORDER);
-  console.log(orders);
+  const history = useHistory();
+  const { data } = useCustomQuery<GetCompletedOrders>(GET_COMPLETED_ORDER);
+  const completedOrders = data?.getCompletedOrders.completedOrders;
 
-  return <OrderHistoryList />;
+  const onClickBackButton = () => {
+    history.goBack();
+  };
+
+  return (
+    <>
+      <HeaderWithBack onClick={onClickBackButton} className="green-header" />
+      <OrderHistoryList orders={completedOrders} />
+    </>
+  );
 };
 
 export default OrderHistory;
