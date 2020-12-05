2db208ed34a7bf91bba5af22de2fa6366aca8250
jongeunk0613 <gladiolus4231@gmail.com>
2020-12-05 15:23:51 +0900

    Feat #146 : 운행종료된 로그 컴포넌트 구현
    - 하나의 로그를 받아서 필요한 정보를 띄워주는 컴포넌트를 구현

--- client/src/components/OrderHistoryList/Log.tsx
+++ client/src/components/OrderHistoryList/Log.tsx
@@ -1,7 +1,53 @@
-import React from 'react';
+import React, { FC } from 'react';
+import { GetCompletedOrders_getCompletedOrders_completedOrders as CompletedOrders } from '@/types/api';
+import styled from '@theme/styled';
+import calcDriveTime from '@utils/calcDriveTime';
 
-const Log = () => {
-  return <>LOG</>;
+const StyledLog = styled.div`
+  border: 1px solid lightgray;
+  border-radius: 0.5rem;
+  margin: 1rem 0;
+  padding: 0.5rem;
+
+  & .completed-order-info {
+    display: flex;
+    margin-bottom: 0.4rem;
+
+    & div:nth-of-type(1) {
+      margin-right: 2.3rem;
+    }
+  }
+
+  & .last-completed-order-info {
+    display: flex;
+
+    & div:nth-of-type(1) {
+      margin-right: 1.5rem;
+    }
+  }
+`;
+
+interface LogProps {
+  order: CompletedOrders;
+}
+
+const Log: FC<LogProps> = ({ order }) => {
+  return (
+    <StyledLog>
+      <div className="completed-order-info">
+        <div>출발지</div> <div>{order.startingPoint.address}</div>
+      </div>
+      <div className="completed-order-info">
+        <div>도착지</div> <div>{order.destination.address}</div>
+      </div>
+      <div className="completed-order-info">
+        <div>결제비</div> <div>{order.amount}원</div>
+      </div>
+      <div className="last-completed-order-info">
+        <div>운행시간</div> <div>{calcDriveTime(order.startedAt, order.completedAt)}</div>
+      </div>
+    </StyledLog>
+  );
 };
 
 export default Log;
