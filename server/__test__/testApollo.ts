903e8d71dbba8502240e3b77823989297cc5eee6
hoo00nn <hoo00nn@naver.com>
2020-12-08 17:22:58 +0900

    Chore #187 : testApollo 파일 추가
    - apollo-server-testing을 이용하여 테스트 하기 위해서 기본 설정 추가

---
+++ server/__test__/testApollo.ts
@@ -0,0 +1,55 @@
+/* eslint-disable no-shadow */
+/* eslint-disable import/no-extraneous-dependencies */
+import { makeExecutableSchema, mergeTypeDefs, mergeResolvers, loadFilesSync } from 'graphql-tools';
+import { ApolloServer } from 'apollo-server-express';
+import { createTestClient, ApolloServerTestClient } from 'apollo-server-testing';
+import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { TEST_USER, TEST_DRIVER } = process.env;
+
+const allTypes = loadFilesSync(path.join(__dirname, '../src/api/**/*.graphql'));
+
+const allResolvers = loadFilesSync(path.join(__dirname, '../src/api/**/*.resolvers.ts'));
+
+const schema = makeExecutableSchema({
+  typeDefs: mergeTypeDefs(allTypes),
+  resolvers: mergeResolvers(allResolvers),
+});
+
+export enum UserType {
+  user = 'user',
+  driver = 'driver',
+}
+
+const RequestUser = {
+  user: {
+    user: {
+      _id: TEST_USER,
+      type: UserType.user,
+    },
+  },
+  driver: {
+    user: {
+      _id: TEST_DRIVER,
+      type: UserType.driver,
+    },
+  },
+};
+
+const client = (type: UserType): ApolloServerTestClient => {
+  const server = new ApolloServer({
+    schema,
+    context: (ctx) => ({
+      ...ctx,
+      req: RequestUser[type],
+      pubsub: jest.fn(),
+    }),
+  });
+
+  return createTestClient(server);
+};
+
+export default client;
