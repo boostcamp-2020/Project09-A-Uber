3f7faa4b9f926cb0191a77b1f74e783c3fc949e8
hoo00nn <hoo00nn@naver.com>
2020-12-08 17:14:21 +0900

    Chore #187 : 몽구스 테스트용 디비 연결설정
    - 테스트 디비 연결 설정함

---
+++ server/__test__/testMongoose.ts
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose';
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const { TEST_DB, DB_HOST } = process.env;
+
+// eslint-disable-next-line no-shadow
+enum message {
+  CONNECT_ERROR = 'mongodb connection error',
+  CONNECT_SUCCEED = 'mongodb connected',
+}
+
+export const connect = () => {
+  const connectDB = () => {
+    mongoose
+      .connect(DB_HOST!, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        dbName: TEST_DB,
+      })
+      .then(() => console.log(message.CONNECT_SUCCEED))
+      .catch((err) => console.error(message.CONNECT_ERROR, err));
+  };
+
+  connectDB();
+};
+
+export const disconnect = () => mongoose.disconnect();
