e388fb95223052bc5e76f1e34c6ca814bbf8149c
kimhakjun <dkd7142@naver.com>
2020-12-09 18:01:14 +0900

    Test #198 : 오더 조회 테스트
    - 오더 조회를 위해 존재하는 orderId, 존재하지않는 orderId mock 데이터 추가
    - 오더 조회 테스트, 존재하지 않는 오더 테스트

--- server/__test__/order.spec.ts
+++ server/__test__/order.spec.ts
@@ -2,7 +2,7 @@ import { gql } from 'apollo-server-express';
 import { connect, disconnect } from './testMongoose';
 import client, { UserType } from './testApollo';
 
-import { completedOrder, newOrderData } from './mock.json';
+import { completedOrder, newOrderData, existOrderId, nonExistOrderId } from './mock.json';
 
 const GET_COMPLETED_ORDERS = gql`
   query GetCompletedOrders {
@@ -32,6 +32,28 @@ const CREATE_ORDER = gql`
   mutation CreateOrder($startingPoint: LocationInfo!, $destination: LocationInfo!) {
     createOrder(startingPoint: $startingPoint, destination: $destination) {
       result
+      orderId
+      error
+    }
+  }
+`;
+
+const GET_ORDER = gql`
+  query GetOrderInfo($orderId: String!) {
+    getOrderInfo(orderId: $orderId) {
+      result
+      order {
+        _id
+        startingPoint {
+          address
+          coordinates
+        }
+        destination {
+          address
+          coordinates
+        }
+        status
+      }
       error
     }
   }
@@ -56,16 +78,16 @@ describe('사용자 오더 테스트', () => {
 
     expect(error).toEqual(null);
 
-    expect(completedOrders[1].amount).toBe(3000);
+    expect(completedOrders[0].amount).toBe(3000);
 
-    expect(completedOrders[0].startingPoint.coordinates.length).toBe(2);
+    expect(completedOrders[1].startingPoint.coordinates.length).toBe(2);
 
-    expect(completedOrders[0].destination.coordinates.length).toBe(2);
+    expect(completedOrders[1].destination.coordinates.length).toBe(2);
 
-    expect(completedOrders[1]).toEqual(completedOrder);
+    expect(completedOrders[0]).toEqual(completedOrder);
   });
 
-  test('오더 생성', async () => {
+  test('오더 생성 테스트', async () => {
     const {
       data: {
         createOrder: { result, error },
@@ -77,6 +99,38 @@ describe('사용자 오더 테스트', () => {
 
     expect(result).toBe('success');
     expect(error).toEqual(null);
+  });
+
+  test('오더 조회 테스트', async () => {
+    const {
+      data: {
+        getOrderInfo: { result, order, error },
+      },
+    } = await query({
+      query: GET_ORDER,
+      variables: {
+        orderId: existOrderId,
+      },
+    });
+    expect(result).toBe('success');
+    expect(order._id).toEqual(existOrderId);
+    expect(error).toEqual(null);
+  });
+
+  test('존재 하지 않는 오더 테스트', async () => {
+    const {
+      data: {
+        getOrderInfo: { result, order, error },
+      },
+    } = await query({
+      query: GET_ORDER,
+      variables: {
+        orderId: nonExistOrderId,
+      },
+    });
+    expect(result).toBe('fail');
+    expect(order).toEqual(null);
+    expect(error).toBe('해당 오더가 존재하지 않습니다.');
   });
 
   afterAll(() => {
