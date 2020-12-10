57842e080c086bb34c0952ab2907d85d8bda4770
chanyeong <cyjo9603@naver.com>
2020-12-10 22:38:56 +0900

    Refactor #229 : 오더 로그 컴포넌트 antd 적용
    - 오더 로그 컴포넌트 antd 적용

--- client/src/components/OrderHistoryList/Log.tsx
+++ client/src/components/OrderHistoryList/Log.tsx
@@ -1,56 +1,23 @@
 import React, { FC } from 'react';
+import { Descriptions } from 'antd';
 import { GetCompletedOrders_getCompletedOrders_completedOrders as CompletedOrders } from '@/types/api';
-import styled from '@theme/styled';
+
 import calcDriveTime from '@utils/calcDriveTime';
-
-const StyledLog = styled.div`
-  border: 1px solid lightgray;
-  border-radius: 0.5rem;
-  margin: 1rem 0;
-  padding: 0.5rem;
-
-  & .completed-order-info {
-    display: flex;
-    margin-bottom: 0.4rem;
-
-    & .completed-order-info-title {
-      margin-right: 2.3rem;
-    }
-  }
-
-  & .last-completed-order-info {
-    display: flex;
-
-    & .completed-order-info-title {
-      margin-right: 1.5rem;
-    }
-  }
-`;
 
 interface LogProps {
   order: CompletedOrders;
 }
 
+const { Item } = Descriptions;
+
 const Log: FC<LogProps> = ({ order }) => {
   return (
-    <StyledLog>
-      <div className="completed-order-info">
-        <div className="completed-order-info-title">출발지 </div>
-        <div>{order.startingPoint.address}</div>
-      </div>
-      <div className="completed-order-info">
-        <div className="completed-order-info-title">도착지 </div>
-        <div>{order.destination.address}</div>
-      </div>
-      <div className="completed-order-info">
-        <div className="completed-order-info-title">결제비 </div>
-        <div>{`${order.amount} 원`}</div>
-      </div>
-      <div className="last-completed-order-info">
-        <div className="completed-order-info-title">운행시간 </div>
-        <div>{calcDriveTime(order.startedAt, order.completedAt)}</div>
-      </div>
-    </StyledLog>
+    <Descriptions size="small" bordered>
+      <Item label="출발지">{order.startingPoint.address}</Item>
+      <Item label="도착지">{order.destination.address}</Item>
+      <Item label="결제비">{`${order.amount} 원`}</Item>
+      <Item label="운행시간">{calcDriveTime(order.startedAt, order.completedAt)}</Item>
+    </Descriptions>
   );
 };
 
