6893ae7d9c28032ebedeb0fc2ee330dc55e2a5c3
chanyeong <cyjo9603@naver.com>
2020-12-10 16:09:54 +0900

    Refactor #217 : 메뉴 Drawer antd 교체
    - 메뉴 Drawer antd 교체

--- client/src/components/HeaderWithMenu/HeaderWithMenu.tsx
+++ client/src/components/HeaderWithMenu/HeaderWithMenu.tsx
@@ -1,8 +1,7 @@
 import React, { FC } from 'react';
 import { Modal } from 'antd-mobile';
-import useToggle from '@hooks/useToggle';
 import { useHistory } from 'react-router-dom';
-import { Layout } from 'antd';
+import { Layout, Drawer, Button } from 'antd';
 import { MenuOutlined } from '@ant-design/icons';
 
 import { useCustomMutation } from '@hooks/useApollo';
@@ -10,6 +9,7 @@ import { LOGOUT } from '@queries/user';
 import { Logout } from '@/types/api';
 import styled from '@theme/styled';
 import { Message } from '@utils/client-message';
+import useModal from '@hooks/useModal';
 
 interface Props {
   className?: string;
@@ -17,7 +17,6 @@ interface Props {
 
 interface StyledProps {
   className: string;
-  isMenuOpen: boolean;
 }
 
 const { Header } = Layout;
@@ -29,29 +28,11 @@ const StyledHeaderWithMenu = styled(Header)<StyledProps>`
   & svg {
     fill: #ffffff;
   }
-
-  & menu {
-    position: absolute;
-    top: 3rem;
-    left: 0;
-    visibility: ${(props) => (props.isMenuOpen ? 'visible' : 'hidden')};
-    width: ${(props) => (props.isMenuOpen ? '50%' : '0')};
-    height: calc(100vh - 3rem);
-    z-index: 1;
-    padding: 1rem 1rem;
-    background-color: #ffffff;
-    border-right: 1px solid ${({ theme }) => theme.PRIMARY};
-    transition: 0.5s;
-
-    & .am-button {
-      position: inherit;
-      font-size: 1.2rem;
-    }
-  }
 `;
 
 const HeaderWithMenu: FC<Props> = ({ className = 'white-header' }) => {
   const history = useHistory();
+  const [visible, onOpen, onClose] = useModal();
   const [logoutMutation] = useCustomMutation<Logout>(LOGOUT, {
     onCompleted: ({ logout }) => {
       if (logout.result === 'success') {
@@ -59,7 +40,6 @@ const HeaderWithMenu: FC<Props> = ({ className = 'white-header' }) => {
       }
     },
   });
-  const [menu, toggleMenu] = useToggle(false);
 
   const onClickCompletedOrders = () => {
     history.push('/orderHistory');
@@ -73,22 +53,23 @@ const HeaderWithMenu: FC<Props> = ({ className = 'white-header' }) => {
   };
 
   return (
-    <StyledHeaderWithMenu className={className} isMenuOpen={menu}>
-      <MenuOutlined data-testID="header-menu-toggle" onClick={toggleMenu} />
-      <menu data-testID="header-menu">
-        <ul>
-          <li>
-            <button type="button" data-testID="history-button" onClick={onClickCompletedOrders}>
-              이용 기록
-            </button>
-          </li>
-          <li>
-            <button type="button" onClick={onClickLogout}>
-              로그아웃
-            </button>
-          </li>
-        </ul>
-      </menu>
+    <StyledHeaderWithMenu className={className}>
+      <MenuOutlined data-testID="header-menu-toggle" onClick={onOpen} />
+      <Drawer
+        data-testID="header-menu"
+        closable={false}
+        onClose={onClose}
+        visible={visible}
+        placement="left"
+      >
+        <Button type="link" onClick={onClickCompletedOrders}>
+          이용 기록
+        </Button>
+        <br />
+        <Button type="link" onClick={onClickLogout}>
+          로그아웃
+        </Button>
+      </Drawer>
     </StyledHeaderWithMenu>
   );
 };
