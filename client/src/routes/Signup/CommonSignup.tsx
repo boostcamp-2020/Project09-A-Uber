88c6a3ed23cc1f89ae5035e54eaa4cb448df42b8
kimhakjun <dkd7142@naver.com>
2020-12-11 15:30:04 +0900

    Refactor #238 회원가입 페이지 antd 마이그레이션
     - formItem 적용
     - vaild 메시지, suffix 적용

--- client/src/routes/Signup/CommonSignup.tsx
+++ client/src/routes/Signup/CommonSignup.tsx
@@ -1,10 +1,8 @@
 import React, { FC } from 'react';
-import { Button, Input } from 'antd';
+import { Button, Input, Tooltip, Avatar, Row, Col, Form } from 'antd';
 import { ExclamationCircleTwoTone, CheckCircleTwoTone } from '@ant-design/icons';
 import { Message } from '@utils/client-message';
 import styled from '@theme/styled';
-// import Input from '@components/Input';
-import ProfileUplodaer from '@components/ProfileUploader';
 import theme from '@/theme';
 
 interface Props {
@@ -32,15 +30,8 @@ const StyledCommonSignup = styled.section`
   display: flex;
   flex-direction: column;
 
-  & > div {
-    margin-bottom: 1.2rem;
-  }
-
-  & > a {
-    cursor: pointer;
+  & .ant-btn {
     margin-top: auto;
-    font-weight: 700;
-    font-size: 0.9rem;
   }
 `;
 
@@ -73,47 +64,89 @@ const CommonSignup: FC<Props> = ({
 }) => {
   return (
     <StyledCommonSignup className={className}>
-      <ProfileUplodaer />
-      <Input
-        value={name}
-        onChange={onChangeName}
-        title="이름"
-        placeholder="이름을 입력해 주세요."
-        suffix={suffix(isName)}
-      />
-      <Input
-        value={email}
-        onChange={onChangeEmail}
-        title="이메일"
-        placeholder="이메일을 입력해 주세요."
-        suffix={suffix(isEmail)}
-      />
-      <Input
-        value={password}
-        onChange={onChangePassword}
-        title="비밀번호"
-        placeholder="비밀번호를 입력해 주세요."
-        type="password"
-        suffix={suffix(isPassword)}
-      />
-      <Input
-        value={passwordRe}
-        onChange={onChangePasswordRe}
-        title="비밀번호 재확인"
-        placeholder="비밀번호를 한번 더 입력해 주세요."
-        type="password"
-        suffix={suffix(isPasswordRe)}
-      />
-      <Input
-        value={phone}
-        onChange={onChangePhone}
-        title="핸드폰 번호"
-        placeholder="핸드폰 번호를 입력해 주세요."
-        suffix={suffix(isEmail)}
-        data-testID="signup-phone"
-      />
+      <Form layout="vertical">
+        <Form.Item
+          preserve
+          name="이름"
+          label="이름"
+          validateStatus={!isName && name.length !== 0 ? 'error' : ''}
+          help={!isName && name.length !== 0 && Message.NameGuidance}
+        >
+          <Input
+            value={name}
+            title="이름"
+            placeholder="이름을 입력해 주세요."
+            onChange={onChangeName}
+            suffix={suffix(isName)}
+            autoComplete="off"
+          />
+        </Form.Item>
+        <Form.Item
+          name="이메일"
+          label="이메일"
+          validateStatus={!isEmail && email.length !== 0 ? 'error' : ''}
+        >
+          <Input
+            value={email}
+            title="이메일"
+            placeholder="이메일을 입력해 주세요."
+            onChange={onChangeEmail}
+            suffix={suffix(isEmail)}
+            autoComplete="off"
+          />
+        </Form.Item>
+        <Form.Item
+          name="비밀번호"
+          label="비밀번호"
+          validateStatus={!isPassword && password.length !== 0 ? 'error' : ''}
+          help={!isPassword && password.length !== 0 && Message.PasswordGuidance}
+        >
+          <Input
+            value={password}
+            type="password"
+            title="비밀번호"
+            placeholder="비밀번호를 입력해 주세요."
+            onChange={onChangePassword}
+            suffix={suffix(isPassword)}
+            autoComplete="off"
+          />
+        </Form.Item>
 
+        <Form.Item
+          name="비밀번호 재확인"
+          label="비밀번호 재확인"
+          validateStatus={!isPasswordRe && passwordRe.length !== 0 ? 'error' : ''}
+          help={!isPasswordRe && passwordRe.length !== 0 && Message.PasswordCheckGuidance}
+        >
+          <Input
+            value={passwordRe}
+            type="password"
+            title="비밀번호 재확인"
+            placeholder="비밀번호를 한번 더 입력해 주세요."
+            onChange={onChangePasswordRe}
+            suffix={suffix(isPasswordRe)}
+            autoComplete="off"
+          />
+        </Form.Item>
+
+        <Form.Item
+          name="핸드폰 번호"
+          label="핸드폰 번호"
+          validateStatus={!isPhone && phone.length !== 0 ? 'error' : ''}
+          help={!isPhone && phone.length !== 0 && Message.PhoneGuidance}
+        >
+          <Input
+            value={phone}
+            title="핸드폰 번호"
+            placeholder="핸드폰 번호를 입력해 주세요."
+            onChange={onChangePhone}
+            suffix={suffix(isPhone)}
+            autoComplete="off"
+          />
+        </Form.Item>
+      </Form>
       <Button
+        block
         type="primary"
         onClick={onClickNextHandler}
         disabled={!isName || !isEmail || !isPassword || !isPasswordRe || !isPhone}
