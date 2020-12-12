b762b1a3095be6fe0467edee7fe6c0483a7bfe96
kimhakjun <dkd7142@naver.com>
2020-12-12 22:36:56 +0900

    Refactor #248 : 일반사용자 회원가입페이지 Form onFinish 적용
     - 회원가입 버튼을 Form 안으로 이동

--- client/src/components/PaymentInfoForm/PaymentInfoForm.tsx
+++ client/src/components/PaymentInfoForm/PaymentInfoForm.tsx
@@ -15,11 +15,15 @@ import { ToggleFocus } from '@components/UserToggle';
 import theme from '@/theme';
 import { ValidateStatus } from 'antd/lib/form/FormItem';
 
-const StyledPaymentInfoForm = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
+const StyledFrom = styled(Form)`
   height: 100%;
+  position: relative;
+
+  & .signup-button {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+  }
 `;
 
 interface Props {
@@ -95,28 +99,23 @@ const PaymentInfoForm: FC<Props> = ({ name, email, password, phone }) => {
   const creditRef3 = useRef<Input>(null);
   const creditRef4 = useRef<Input>(null);
 
-  const onSubmit = useCallback(
-    (e: React.FormEvent) => {
-      e.preventDefault();
-      const paymentInfo = {
-        bank,
-        creditNumber: `${cardNumber1}-${cardNumber2}-${cardNumber3}-${cardNumber4}`,
-        expiryDate,
-        cvc: Number(cvc),
-      };
-      signinUserMutation({
-        variables: {
-          name,
-          email,
-          password,
-          phone,
-          payment: paymentInfo,
-        },
-      });
-    },
-
-    [bank, cardNumber1, cardNumber2, cardNumber3, cardNumber4, expiryDate, cvc],
-  );
+  const onSubmit = useCallback(() => {
+    const paymentInfo = {
+      bank,
+      creditNumber: `${cardNumber1}-${cardNumber2}-${cardNumber3}-${cardNumber4}`,
+      expiryDate,
+      cvc: Number(cvc),
+    };
+    signinUserMutation({
+      variables: {
+        name,
+        email,
+        password,
+        phone,
+        payment: paymentInfo,
+      },
+    });
+  }, [bank, cardNumber1, cardNumber2, cardNumber3, cardNumber4, expiryDate, cvc]);
 
   useEffect(() => {
     if (isCardNumber1Valid) {
@@ -141,144 +140,146 @@ const PaymentInfoForm: FC<Props> = ({ name, email, password, phone }) => {
   };
 
   return (
-    <StyledPaymentInfoForm>
-      <Form layout="vertical">
-        <Form.Item name="카드 회사" label="카드 회사">
-          <Select placeholder="카드 회사를 선택해 주세요" onChange={onChangeBank}>
-            {Banks.map((bank) => (
-              <Option key={bank} value={bank}>
-                {bank}
-              </Option>
-            ))}
-          </Select>
-        </Form.Item>
-        <Row align="bottom" gutter={5}>
-          <Col span={6}>
-            <Form.Item
-              name="카드 번호1"
-              label="카드 번호"
-              validateStatus={validateStatus(isCardNumber1Valid, cardNumber1)}
-            >
-              <Input
-                value={cardNumber1}
-                title="카드 번호"
-                onChange={onChangeCardNumber1}
-                suffix={suffix(isCardNumber1Valid)}
-                autoComplete="off"
-                data-testId="signup-card1"
-                maxLength={USER.CARD_MAX_LENGTH}
-              />
-            </Form.Item>
-          </Col>
-          <Col span={6}>
-            <Form.Item
-              name="카드 번호2"
-              validateStatus={validateStatus(isCardNumber2Valid, cardNumber2)}
-            >
-              <Input
-                value={cardNumber2}
-                title="카드 번호"
-                onChange={onChangeCardNumber2}
-                suffix={suffix(isCardNumber2Valid)}
-                autoComplete="off"
-                data-testId="signup-card2"
-                maxLength={USER.CARD_MAX_LENGTH}
-                ref={creditRef2}
-              />
-            </Form.Item>
-          </Col>
-          <Col span={6}>
-            <Form.Item
-              name="카드 번호3"
-              validateStatus={validateStatus(isCardNumber3Valid, cardNumber3)}
-            >
-              <Input
-                value={cardNumber3}
-                title="카드 번호"
-                onChange={onChangeCardNumber3}
-                suffix={suffix(isCardNumber3Valid)}
-                autoComplete="off"
-                data-testId="signup-card3"
-                maxLength={USER.CARD_MAX_LENGTH}
-                ref={creditRef3}
-              />
-            </Form.Item>
-          </Col>
-          <Col span={6}>
-            <Form.Item
-              name="카드 번호4"
-              validateStatus={validateStatus(isCardNumber4Valid, cardNumber4)}
-            >
-              <Input
-                value={cardNumber4}
-                title="카드 번호"
-                type="password"
-                onChange={onChangeCardNumber4}
-                suffix={suffix(isCardNumber4Valid)}
-                autoComplete="off"
-                data-testId="signup-card1"
-                maxLength={USER.CARD_MAX_LENGTH}
-                ref={creditRef4}
-              />
-            </Form.Item>
-          </Col>
-        </Row>
-        <Row gutter={5}>
-          <Col span={6}>
-            <Form.Item
-              name="만료일"
-              label="만료일"
-              validateStatus={validateStatus(isExpiryDateValid, expiryDate)}
-              help={helpMessage(isExpiryDateValid, expiryDate, Message.ExpiryDateGuidance)}
-            >
-              <Input
-                value={expiryDate}
-                title="만료일"
-                onChange={onChangeExpiryDate}
-                suffix={suffix(isExpiryDateValid)}
-                autoComplete="off"
-                data-testId="signup-expiry-date"
-                maxLength={USER.EXPIRY_DATA_MAX_LENGTH}
-              />
-            </Form.Item>
-          </Col>
-        </Row>
-        <Row gutter={5}>
-          <Col span={6}>
-            <Form.Item name="CVC" label="CVC" validateStatus={validateStatus(isCvcValid, cvc)}>
-              <Input
-                value={cvc}
-                title="CVC"
-                type="password"
-                onChange={onChangeCvc}
-                suffix={suffix(isCvcValid)}
-                autoComplete="off"
-                data-testId="signup-cvc"
-                maxLength={USER.CVC_MAX_NUM}
-              />
-            </Form.Item>
-          </Col>
-        </Row>
-      </Form>
-
-      <Button
-        type="primary"
-        onClick={onSubmit}
-        loading={loading}
-        disabled={
-          !bank ||
-          !isCardNumber1Valid ||
-          !isCardNumber2Valid ||
-          !isCardNumber3Valid ||
-          !isCardNumber4Valid ||
-          !isExpiryDateValid ||
-          !isCvcValid
-        }
-        data-testID="signup-user-submit"
-      >
-        회원가입
-      </Button>
-    </StyledPaymentInfoForm>
+    <StyledFrom layout="vertical" onFinish={onSubmit} style={{ height: '100%' }}>
+      <Form.Item name="카드 회사" label="카드 회사">
+        <Select placeholder="카드 회사를 선택해 주세요" onChange={onChangeBank}>
+          {Banks.map((bank) => (
+            <Option key={bank} value={bank}>
+              {bank}
+            </Option>
+          ))}
+        </Select>
+      </Form.Item>
+      <Row align="bottom" gutter={5}>
+        <Col span={6}>
+          <Form.Item
+            name="카드 번호1"
+            label="카드 번호"
+            validateStatus={validateStatus(isCardNumber1Valid, cardNumber1)}
+          >
+            <Input
+              value={cardNumber1}
+              title="카드 번호"
+              onChange={onChangeCardNumber1}
+              suffix={suffix(isCardNumber1Valid)}
+              autoComplete="off"
+              data-testId="signup-card1"
+              maxLength={USER.CARD_MAX_LENGTH}
+            />
+          </Form.Item>
+        </Col>
+        <Col span={6}>
+          <Form.Item
+            name="카드 번호2"
+            validateStatus={validateStatus(isCardNumber2Valid, cardNumber2)}
+          >
+            <Input
+              value={cardNumber2}
+              title="카드 번호"
+              onChange={onChangeCardNumber2}
+              suffix={suffix(isCardNumber2Valid)}
+              autoComplete="off"
+              data-testId="signup-card2"
+              maxLength={USER.CARD_MAX_LENGTH}
+              ref={creditRef2}
+            />
+          </Form.Item>
+        </Col>
+        <Col span={6}>
+          <Form.Item
+            name="카드 번호3"
+            validateStatus={validateStatus(isCardNumber3Valid, cardNumber3)}
+          >
+            <Input
+              value={cardNumber3}
+              title="카드 번호"
+              onChange={onChangeCardNumber3}
+              suffix={suffix(isCardNumber3Valid)}
+              autoComplete="off"
+              data-testId="signup-card3"
+              maxLength={USER.CARD_MAX_LENGTH}
+              ref={creditRef3}
+            />
+          </Form.Item>
+        </Col>
+        <Col span={6}>
+          <Form.Item
+            name="카드 번호4"
+            validateStatus={validateStatus(isCardNumber4Valid, cardNumber4)}
+          >
+            <Input
+              value={cardNumber4}
+              title="카드 번호"
+              type="password"
+              onChange={onChangeCardNumber4}
+              suffix={suffix(isCardNumber4Valid)}
+              autoComplete="off"
+              data-testId="signup-card1"
+              maxLength={USER.CARD_MAX_LENGTH}
+              ref={creditRef4}
+            />
+          </Form.Item>
+        </Col>
+      </Row>
+      <Row gutter={5}>
+        <Col span={6}>
+          <Form.Item
+            name="만료일"
+            label="만료일"
+            validateStatus={validateStatus(isExpiryDateValid, expiryDate)}
+            help={helpMessage(isExpiryDateValid, expiryDate, Message.ExpiryDateGuidance)}
+          >
+            <Input
+              value={expiryDate}
+              title="만료일"
+              onChange={onChangeExpiryDate}
+              suffix={suffix(isExpiryDateValid)}
+              autoComplete="off"
+              data-testId="signup-expiry-date"
+              maxLength={USER.EXPIRY_DATA_MAX_LENGTH}
+            />
+          </Form.Item>
+        </Col>
+      </Row>
+      <Row gutter={5}>
+        <Col span={6}>
+          <Form.Item name="CVC" label="CVC" validateStatus={validateStatus(isCvcValid, cvc)}>
+            <Input
+              value={cvc}
+              title="CVC"
+              type="password"
+              onChange={onChangeCvc}
+              suffix={suffix(isCvcValid)}
+              autoComplete="off"
+              data-testId="signup-cvc"
+              maxLength={USER.CVC_MAX_NUM}
+            />
+          </Form.Item>
+        </Col>
+      </Row>
+      <Row className="signup-button">
+        <Col span={24}>
+          <Button
+            block
+            type="primary"
+            htmlType="submit"
+            loading={loading}
+            disabled={
+              !bank ||
+              !isCardNumber1Valid ||
+              !isCardNumber2Valid ||
+              !isCardNumber3Valid ||
+              !isCardNumber4Valid ||
+              !isExpiryDateValid ||
+              !isCvcValid
+            }
+            data-testID="signup-user-submit"
+          >
+            회원가입
+          </Button>
+        </Col>
+      </Row>
+    </StyledFrom>
   );
 };
 
