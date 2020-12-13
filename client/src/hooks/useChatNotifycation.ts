cf46faaec813239615f31a865c1db0532cbebc5e
chanyeong <cyjo9603@naver.com>
2020-12-13 14:08:36 +0900

    Feat #noissue : 채팅 알림 최대 갯수 제한
    - 채팅 알림 최대 갯수 5개로 제한

--- client/src/hooks/useChatNotifycation.ts
+++ client/src/hooks/useChatNotifycation.ts
@@ -5,12 +5,17 @@ import { SUB_CHAT } from '@queries/chat';
 import { SubChat } from '@/types/api';
 
 const CHAT_DURATION = 1;
+const CHAT_MAX_COUNT = 5;
 
 const useChatNotifycation = (id: string) => {
   const subscriptionResult = useSubscription<SubChat>(SUB_CHAT, {
     variables: { chatId: id },
     onSubscriptionData: ({ subscriptionData }) => {
       if (subscriptionData.data?.subChat.chat) {
+        message.config({
+          duration: CHAT_DURATION,
+          maxCount: CHAT_MAX_COUNT,
+        });
         message.info(subscriptionData.data.subChat.chat.content, CHAT_DURATION);
       }
     },
