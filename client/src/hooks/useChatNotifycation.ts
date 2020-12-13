9602231d2e38987c907db9b2dc7148c1afee93f5
chanyeong <cyjo9603@naver.com>
2020-12-13 11:47:00 +0900

    Refactor #noissue : 채팅 알림 매직넘버 제거
    - 채팅 알림 매직넘버 제거

--- client/src/hooks/useChatNotifycation.ts
+++ client/src/hooks/useChatNotifycation.ts
@@ -4,12 +4,14 @@ import { message } from 'antd';
 import { SUB_CHAT } from '@queries/chat';
 import { SubChat } from '@/types/api';
 
+const CHAT_DURATION = 1;
+
 const useChatNotifycation = (id: string) => {
   const subscriptionResult = useSubscription<SubChat>(SUB_CHAT, {
     variables: { chatId: id },
     onSubscriptionData: ({ subscriptionData }) => {
       if (subscriptionData.data?.subChat.chat) {
-        message.info(subscriptionData.data.subChat.chat.content, 1);
+        message.info(subscriptionData.data.subChat.chat.content, CHAT_DURATION);
       }
     },
   });
